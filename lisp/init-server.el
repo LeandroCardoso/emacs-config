d657f8d4dc3147b363acca0a399ef46cc4e0b399
Leandro Cardoso <leandrocardoso@gmail.com>
2016-06-07 16:02:16 -0300

    fix emacs server not starting at startup

--- lisp/init-server.el
+++ lisp/init-server.el
@@ -1,5 +1,5 @@
 ;; Start the emacs server needed by the emacsclient
 (require 'server)
-(unless (server-running-p)
+(unless (eq (server-running-p) t)
   (server-start)
   (message "Server started"))
