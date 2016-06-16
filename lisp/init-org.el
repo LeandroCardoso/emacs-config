4fe7917b401a79ecb1b8db1e10bb93ac023da867
Leandro Cardoso <leandrocardoso@gmail.com>
2016-06-16 13:41:20 -0300

    set org-tabs

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -13,7 +13,7 @@
 (setq org-M-RET-may-split-line '((default . nil)))  ; don't split the line at the cursor position when ALT+ENTER
 (setq org-completion-use-ido t) ; use ido completion wherever possible
 (setq org-ellipsis 'org-ellipsis); print ellipsis '...' with custom face
-(setq org-imenu-depth 10) ; maximum level for Imenu access
+(setq org-imenu-depth 6) ; maximum level for Imenu access
 (setq org-level-color-stars-only t) ; fontify only the stars in each headline
 (setq org-outline-path-complete-in-steps nil) ; I don't need this because I have ido-mode
 (setq org-special-ctrl-a/e t) ; special headline handling
@@ -22,6 +22,8 @@
 (setq org-src-window-setup 'current-window); show edit buffer in the current window
 (setq org-startup-folded nil) ; open org files in unfolded mode
 (setq org-startup-truncated nil) ; don't set `truncate-lines', this break long tables
+(setq org-tags-column (- fill-column)) ; align tags at the right margin
+(setq org-tags-sort-function 'string<) ; align tags using alphabetic order
 
 ;; org-mode outside org-mode
 (defvar org-out-keymap
