970ffc494eeaac4006d3928ef8c2f3328c4bea77
Leandro Cardoso <leandrocardoso@gmail.com>
2016-06-21 10:59:32 -0300

    initial newcomment.el

--- init.el
+++ init.el
@@ -8,7 +8,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(comment-column 0)
  '(custom-buffer-done-kill t)
  '(custom-raised-buttons nil)
  '(delete-selection-mode t nil (delsel))
