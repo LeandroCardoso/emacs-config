35066b1ec0662d01c3977d3451fb48146c7b0044
Leandro Cardoso <leandrocardoso@gmail.com>
2016-07-07 17:06:48 -0300

    fix compilation derived modes (like grep) firing the msvs-copy-bin-to-drive

--- lisp/init-dbd.el
+++ lisp/init-dbd.el
@@ -1,15 +1,20 @@
 (defun set-dbd-environment ()
   "Set my DBD environment"
   (interactive)
+
   ;; proxy
   (setq url-proxy-services '(("http" . "localhost:3128")
                              ("https" . "localhost:3128")))
 
+  
   ;; Copy binaries to the pen drive after a successful compilation
   (add-hook 'compilation-finish-functions
             (lambda (BUFFER STATUS)
-              (hack-dir-local-variables-non-file-buffer)
-              (msvs-copy-bin-to-drive)))
+              ;; compilation-mode has some derived modes, like grep-mode, that fire this hook too,
+              ;; we should avoid it and only proceed if it is a real compilation.
+              (when (eq (buffer-local-value 'major-mode BUFFER) 'compilation-mode)
+                (hack-dir-local-variables-non-file-buffer)
+                (msvs-copy-bin-to-drive))))
   )
 
 
