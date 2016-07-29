b7b3293d162b08db572fc43caf39d255f16fd728
Leandro Cardoso <leandrocardoso@gmail.com>
2016-07-29 09:57:28 -0300

    tweaked display of minor modes and buffer name

--- lisp/init-smart-mode-line.el
+++ lisp/init-smart-mode-line.el
@@ -1,10 +1,12 @@
 (when (require 'smart-mode-line nil t)
+  (require 'rich-minority nil t)
+  
   (setq sml/theme nil)
-  (setq sml/col-number "%c")
-  (setq sml/name-width 30)
+  (setq sml/col-number-format "%c")
+  (setq sml/name-width '(20 . 50))
   (setq sml/no-confirm-load-theme t)
-  (setq sml/pos-minor-modes-separator "]")
-  (setq sml/pre-modes-separator "[")
+  ;; (setq sml/pos-minor-modes-separator "]")
+  ;; (setq sml/pre-modes-separator " [")
   (setq sml/shorten-mode-string "")
   (setq sml/show-file-name nil)
   (setq sml/vc-mode-show-backend t)
@@ -14,4 +16,7 @@
                                                   (if (project-name)
                                                       (concat ":" (project-name) ":")
                                                     (match-string 0 str)))) t)
+
+  (setq rm-whitelist (mapconcat 'identity '("company-.*") "\\|"))
+  
   (smart-mode-line-enable))
