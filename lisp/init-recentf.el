c2eeca63bec1579d92fa29f137f93330d1465374
Leandro Cardoso <leandrocardoso@gmail.com>
2016-09-14 13:22:28 -0300

    added other-window and other-frame functions and key-bindings

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -7,9 +7,32 @@
 (run-at-time nil 300 'recentf-save-list)
 
 (defun recentf-find-file ()
-  "Switch to a buffer visiting file from the recently visited file list from the `recentf-list'.
-This function works better with a completion enhancement like `ido-ubiquitous' and `flx-ido-mode' enable."
+  "Edit file from the `recentf-list'.
+
+This function works better with a `completing-read' enhancement like `ido'."
   (interactive)
   (find-file (completing-read "Find recent file: " recentf-list nil t)))
 
-(global-set-key (kbd "C-x C-r") 'recentf-find-file) ; default is ido-find-file-read-only
+(defun recentf-find-file-other-window ()
+  "Edit file from the `recentf-list', in another window.
+
+This function works better with a `completing-read' enhancement like `ido'."
+  (interactive)
+  (find-file-other-window (completing-read "Find recent file: " recentf-list nil t)))
+
+(defun recentf-find-file-other-frame ()
+  "Edit file from the `recentf-list', in another frame.
+
+This function works better with a `completing-read' enhancement like `ido'."
+  (interactive)
+  (find-file-other-frame (completing-read "Find recent file: " recentf-list nil t)))
+
+(global-set-key (kbd "C-x C-r") 'recentf-find-file) ; default is find-file-read-only
+
+;; other window
+(global-set-key (kbd "C-x 4 C-r") 'recentf-find-file-other-window) ; default is find-file-read-only-other-window
+(global-set-key (kbd "C-x 4 r") 'recentf-find-file-other-window) ; default is find-file-read-only-other-window
+
+;; other frame
+(global-set-key (kbd "C-x 5 C-r") 'recentf-find-file-other-frame) ; default is find-file-read-only-other-frame
+(global-set-key (kbd "C-x 5 r") 'recentf-find-file-other-frame) ; default is find-file-read-only-other-frame
