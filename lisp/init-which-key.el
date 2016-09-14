f5ea33e954420727790bd349e44a57d110315966
Leandro Cardoso <leandrocardoso@gmail.com>
2016-09-14 09:17:01 -0300

    set secondary delay

--- lisp/init-which-key.el
+++ lisp/init-which-key.el
@@ -1,4 +1,5 @@
 (when (require 'which-key nil t)
+  (setq which-key-idle-secondary-delay 0)
   (setq which-key-lighter nil)
   (setq which-key-max-description-length 50)
   (setq which-key-sort-order 'which-key-local-then-key-order)
