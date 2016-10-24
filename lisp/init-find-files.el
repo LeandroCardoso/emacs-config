73b61302a0cc891f78f3c8013a665a3d0b7aa7c2
Leandro Cardoso <leandrocardoso@gmail.com>
2016-10-24 11:33:01 -0200

    Add function find-file-sudo

--- lisp/init-find-files.el
+++ lisp/init-find-files.el
@@ -1,3 +1,11 @@
 (setq ff-case-fold-search t)
 
+(unless (eq system-type 'windows-nt)
+  (defun find-file-sudo ()
+    "Reopen the current file as root, preserving point position."
+    (interactive)
+    (let ((p (point)))
+      (find-alternate-file (concat "/sudo::" buffer-file-name))
+      (goto-char p))))
+
 (global-set-key (kbd "C-M-o") 'ff-find-other-file) ;; default is split-line
