3cf1d0eddb97f1db3b63cc662852cd4ef54e115c
Leandro Cardoso <leandrocardoso@gmail.com>
2016-11-11 09:04:21 -0200

    set scroll to not recenter always

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -1,5 +1,7 @@
 (setq frame-resize-pixelwise t)
 (setq highlight-nonselected-windows t)
+(setq scroll-conservatively 5)
+(setq scroll-preserve-screen-position t)
 (setq truncate-partial-width-windows nil)
 (setq undo-limit 800000)
 (setq undo-strong-limit undo-limit)
