b34e11819c6b91276bf6a122245c26eb24d2a408
Leandro Cardoso <lcardoso@rdisoftware.com>
2017-06-28 13:54:34 -0300

    Set some RDI scripts to bat-mode

--- lisp/rdi.el
+++ lisp/rdi.el
@@ -1,4 +1,5 @@
 (add-to-list 'auto-mode-alist '("\\.nps\\'" . javascript-mode))
+(add-to-list 'auto-mode-alist '("start\\.np" . bat-mode))
 
 ;; Use TABs with XML files
 (add-hook 'nxml-mode-hook (lambda () (setq indent-tabs-mode t)))
