94aea9d967f22e91d23262def7601fa5138a3538
Leandro Cardoso <lcardoso@rdisoftware.com>
2017-06-30 11:32:05 -0300

    xref: Removed key

--- lisp/init-xref.el
+++ lisp/init-xref.el
@@ -1,5 +1,3 @@
-(global-set-key (kbd "C-M-,") 'xref-find-references)
-
 (with-eval-after-load "xref"
   (define-key xref--xref-buffer-mode-map (kbd "<tab>") 'xref-next-line)
   (define-key xref--xref-buffer-mode-map (kbd "<backtab>") 'xref-prev-line))
