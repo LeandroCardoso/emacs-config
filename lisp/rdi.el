6831148b799f9f03db6f0f35c54e80b74d62625f
Leandro Cardoso <lcardoso@rdisoftware.com>
2017-07-03 15:25:12 -0300

    RDI: Use TABs with javascript files

--- lisp/rdi.el
+++ lisp/rdi.el
@@ -3,6 +3,8 @@
 
 ;; Use TABs with XML files
 (add-hook 'nxml-mode-hook (lambda () (setq indent-tabs-mode t)))
+;; Use TABs with javascript files
+(add-hook 'js-mode-hook (lambda () (setq indent-tabs-mode t)))
 
 ;; There are some c++ files using .c extension.
 (add-to-list 'auto-mode-alist '("\\.c\\'" . c++-mode))
