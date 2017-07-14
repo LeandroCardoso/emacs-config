c8f81a1733e01625079081e11c2fe49146a2adec
lcardoso <lcardoso@rdisoftware.com>
2017-07-14 15:55:12 -0300

    ediff: added global keymap and setting to restore frame after quit

--- lisp/init-ediff.el
+++ lisp/init-ediff.el
@@ -3,9 +3,32 @@
 (setq ediff-split-window-function 'split-window-horizontally)
 (setq ediff-window-setup-function 'ediff-setup-windows-plain) ; everything in one frame
 
+;; Restore frame and windows after quit an ediff session
+(winner-mode)
+(add-hook 'ediff-after-quit-hook-internal 'winner-undo)
+
 ;; I don't know why I need such a complex code to set the keymap with ediff-mult, but this works.
 (with-eval-after-load "ediff-mult"
   (add-hook 'ediff-meta-buffer-keymap-setup-hook
             (lambda ()
               (define-key ediff-meta-buffer-map (kbd "<tab>") 'ediff-next-meta-item)
               (define-key ediff-meta-buffer-map (kbd "<backtab>") 'ediff-previous-meta-item))))
+
+;; global keymap
+(defvar ediff-global-keymap
+  (let ((map (make-sparse-keymap)))
+    (define-key map "b" 'ediff-buffers)
+    (define-key map "B" 'ediff-buffers3)
+    (define-key map "c" 'ediff-current-file)
+    (define-key map "d" 'ediff-directories)
+    (define-key map "D" 'ediff-directories3)
+    (define-key map "f" 'ediff-files)
+    (define-key map "F" 'ediff-files3)
+    (define-key map "h" 'ediff-documentation)
+    (define-key map "k" 'ediff-backup)
+    (define-key map "r" 'ediff-show-registry)
+    map)
+  "Keymap for global ediff commands")
+
+(defalias 'ediff-global-keymap ediff-global-keymap)
+(global-set-key (kbd "C-x M-e") 'ediff-global-keymap)
