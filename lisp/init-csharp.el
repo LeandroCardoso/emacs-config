4e1cc23f72f84f6f87e126a6159412fca1bbc230
lcardoso <lcardoso@rdisoftware.com>
2017-07-17 15:51:01 -0300

    csharp: added omnisharp settings

--- lisp/init-csharp.el
+++ lisp/init-csharp.el
@@ -1,3 +1,16 @@
-;; auto modes
+;; csharp
 (with-eval-after-load "csharp"
-  (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-mode)))
+  (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-mode))
+
+  ;; omnisharp
+  (when (require 'omnisharp nil t)
+    (setq omnisharp-server-executable-path (concat user-emacs-directory "omnisharp/OmniSharp.exe"))
+    (add-hook 'csharp-mode-hook 'omnisharp-mode)
+
+    ;; omnisharp + company
+    (with-eval-after-load "company"
+      (add-to-list 'company-backends 'company-omnisharp)
+      (setq omnisharp-company-match-type 'company-match-flx)
+      (setq omnisharp-imenu-support t))
+    )
+  )
