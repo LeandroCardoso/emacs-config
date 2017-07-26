f4e8edb4864a2514ed97e106a5acc8b8cd48b2b3
lcardoso <lcardoso@rdisoftware.com>
2017-07-26 12:53:12 -0300

    time: added function display-time-mode-dwim and adviced toggle-frame-fullscreen

--- lisp/init-time.el
+++ lisp/init-time.el
@@ -1,2 +1,15 @@
+(defun display-time-mode-dwim ()
+  "Enable `display-time-mode' in text teminals or when any frame
+is in fullscreen in a graphical display."
+  (display-time-mode
+   (if (or (seq-some #'(lambda (frame)
+                         (memq (frame-parameter frame 'fullscreen) '(fullscreen fullboth)))
+                     (frame-list))
+           (not (display-graphic-p)))
+       1
+     0)))
+
 (setq display-time-24hr-format t)
 (setq display-time-load-average-threshold 0.5)
+
+(advice-add 'toggle-frame-fullscreen :after #'display-time-mode-dwim)
