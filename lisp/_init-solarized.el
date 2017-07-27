7893a7515c03cece06dd394fc3770787d403c6b8
Leandro Cardoso <leandrocardoso@gmail.com>
2017-07-27 03:20:40 -0300

    solarized: Added mode-line faces customization

--- lisp/_init-solarized.el
+++ lisp/_init-solarized.el
@@ -3,8 +3,19 @@
   (setq solarized-scale-org-headlines nil)
   (setq solarized-use-more-italic t)
   (load-theme 'solarized-dark t)
-  (set-face-background 'cursor "#b58900") ;yellow
+  (set-face-background 'cursor "#b58900")              ;yellow
   (set-face-foreground 'mode-line-buffer-id "#b58900") ;yellow
+  (set-face-attribute 'mode-line nil
+                      :background "#073642"
+                      :underline nil
+                      :box `(:line-width 2
+                                         :color ,(face-background 'mode-line)))
+
+  (set-face-attribute 'mode-line-inactive nil
+                      :background (face-background 'mode-line)
+                      :underline nil
+                      :box `(:line-width 2
+                                         :color ,(face-background 'mode-line)))
   ;; TODO packages
   ;; TODO some grep improvements
   )
