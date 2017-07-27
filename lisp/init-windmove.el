2b53278e012552960726677a30953ad41e4439a4
Leandro Cardoso <leandrocardoso@gmail.com>
2017-07-27 16:17:56 -0300

    windmove: added support to framemove

--- lisp/init-windmove.el
+++ lisp/init-windmove.el
@@ -1,2 +1,6 @@
-; enable windmove - CTRL is only modifier not used by org-mode
+;; enable windmove - CTRL is only modifier not used by org-mode
 (windmove-default-keybindings 'ctrl)
+
+;; framemove is a windmove extension for frames.
+(when (require 'framemove nil t)
+  (setq framemove-hook-into-windmove t))
