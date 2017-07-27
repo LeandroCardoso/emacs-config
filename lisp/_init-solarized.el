6f21da3bd24b16bc25677a6ab4c7d980e2a6dc4d
Leandro Cardoso <leandrocardoso@gmail.com>
2017-07-27 10:41:08 -0300

    set mode-line faces to be more attractive

--- lisp/_init-solarized.el
+++ lisp/_init-solarized.el
@@ -6,16 +6,17 @@
   (set-face-background 'cursor "#b58900")              ;yellow
   (set-face-foreground 'mode-line-buffer-id "#b58900") ;yellow
   (set-face-attribute 'mode-line nil
-                      :background "#073642"
+                      :background "#073642" ;s-base03
+                      :overline "#586e75"   ;s-base02
                       :underline nil
                       :box `(:line-width 2
-                                         :color ,(face-background 'mode-line)))
-
+                                         :color ,(face-background 'mode-line)
+                                         :style unspecified))
   (set-face-attribute 'mode-line-inactive nil
                       :background (face-background 'mode-line)
+                      :overline nil
                       :underline nil
-                      :box `(:line-width 2
-                                         :color ,(face-background 'mode-line)))
+                      :box (face-attribute 'mode-line :box))
   ;; TODO packages
   ;; TODO some grep improvements
   )
--- lisp/init-monokai.el
+++ lisp/init-monokai.el
@@ -3,12 +3,12 @@
   (set-face-background 'cursor monokai-red)
   (set-face-foreground 'fringe monokai-gray)
   ;; monokai has some really weird mode-line faces
-  (set-face-attribute 'mode-line-inactive nil
-                      :background (face-background 'mode-line)
+  (set-face-attribute 'mode-line nil
+                      :overline monokai-gray
                       :box `(:line-width 2
                                          :color ,(face-background 'mode-line)
                                          :style unspecified))
-  (set-face-attribute 'mode-line nil
-                      :box `(:line-width 2
-                                         :color ,monokai-gray
-                                         :style unspecified)))
+  (set-face-attribute 'mode-line-inactive nil
+                      :background (face-background 'mode-line)
+                      :overline nil
+                      :box (face-attribute 'mode-line :box)))
