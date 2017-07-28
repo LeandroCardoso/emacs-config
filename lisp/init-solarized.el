1eb41dd435d3e3a919947bbfb189220d17fcddde
Leandro Cardoso <leandrocardoso@gmail.com>
2017-07-28 16:56:02 -0300

    Enabled solarized theme instead of monokai. Also enable hl-line.

--- lisp/init-solarized.el
+++ lisp/init-solarized.el
@@ -3,11 +3,12 @@
   (setq solarized-scale-org-headlines nil)
   (setq solarized-use-more-italic t)
   (load-theme 'solarized-dark t)
-  (set-face-background 'cursor "#b58900")              ;yellow
+  (set-face-background 'cursor "#268bd2")              ;blue
   (set-face-foreground 'mode-line-buffer-id "#b58900") ;yellow
+  (set-face-foreground 'minibuffer-prompt (face-foreground 'mode-line-buffer-id))
   (set-face-attribute 'mode-line nil
-                      :background "#073642" ;s-base03
-                      :overline "#586e75"   ;s-base02
+                      :background "#073642"            ;s-base02
+                      :overline (face-foreground 'mode-line-buffer-id)
                       :underline nil
                       :box `(:line-width 2
                                          :color ,(face-background 'mode-line)
@@ -17,6 +18,11 @@
                       :overline nil
                       :underline nil
                       :box (face-attribute 'mode-line :box))
+
+  ;; These should be fixed in upstream
+  (set-face-foreground 'magit-blame-name "#8B2C02") ;orange-d
+  (set-face-foreground 'magit-blame-date "#8B2C02") ;orange-d
+  
   ;; TODO packages
   ;; TODO some grep improvements
   )
