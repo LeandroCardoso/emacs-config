fdd3ea9e6b68381d5834e3cfed0c67508663a6c6
Leandro Cardoso <leandrocardoso@gmail.com>
2017-08-02 13:57:44 -0300

    nxml: added xml-pretty-print function

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -1,2 +1,10 @@
 (setq nxml-child-indent 4)
 (setq nxml-slash-auto-complete-flag t)
+
+;; TODO use the region marked instead of the whole buffer.
+(defun xml-pretty-print ()
+  "Simple-minded pretty printer for XML.
+Re-indents the code and inserts newlines.
+You might want to turn on `auto-fill-mode' to get better results."
+  (interactive)
+  (sgml-pretty-print (point-min) (point-max)))
