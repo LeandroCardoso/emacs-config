9cfb91946de91e0d489de78d425b97237122ea0e
Leandro Cardoso <leandrocardoso@gmail.com>
2017-09-01 14:40:22 -0300

    rdi: added np6 production log mode

--- lisp/rdi.el
+++ lisp/rdi.el
@@ -8,7 +8,7 @@
 ;; There are some c++ files using .c extension.
 (add-to-list 'auto-mode-alist '("\\.c\\'" . c++-mode))
 
-;; np6-log-mode
+;; np6 log mode
 (modify-coding-system-alist 'file "\\.log\\'" 'prefer-utf-8-dos)
 (define-generic-mode np6-log-mode ;; MODE
   nil                             ;; COMMENT-LIST
@@ -29,6 +29,23 @@
 ;; strings.
 (require 'mode-local)
 (setq-mode-local np6-log-mode font-lock-keywords-only t)
+
+;; np6 production log mode
+(define-generic-mode np6-prod-log-mode ;; MODE
+  nil                             ;; COMMENT-LIST
+  nil                             ;; KEYWORD-LIST
+  '(("\\(INFO\\|FINEST\\|DEBUG\\)" . font-lock-function-name-face)
+    ("WARNING" . compilation-warning-face)
+    ("\\(ERROR\\|FATAL\\)" . compilation-error-face)
+    ("^[0-9- :]*;" . font-lock-comment-face) ; timestamp
+    ("\\[com.*\\] Thread: .*$" . font-lock-comment-face)
+    )                             ;; FONT-LOCK-LIST
+  '("\\(newposv6\\|np6[a-z]*\\)-0\\.0\\.log") ;; AUTO-MODE-LIST
+  (list
+   (lambda ()
+     (setq global-auto-revert-ignore-buffer t))
+   ) ;; FUNCTION-LIST
+  )
 
 ;; .np6 and .npsharp mode
 (define-generic-mode np6-mode ;; MODE
