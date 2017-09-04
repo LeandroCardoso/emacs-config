cbf2219d3822e87cc98af2c5ced08b642959d64b
Leandro Cardoso <leandrocardoso@gmail.com>
2017-09-04 13:46:54 -0300

    shell: removed deprecated functions

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -1,20 +1,6 @@
 ;; Display shell buffers in the same window
 (add-to-list 'display-buffer-alist
-             '("^\\*shell\\*" . ((display-buffer-reuse-window display-buffer-same-window)
-                                 (reusable-frames . t))))
-
-(defun shell-other-window (&optional buffer)
-  "Like `shell', but put buffer in another window."
-  (interactive)
-  (let (buf (call-interactively 'shell))
-    (switch-to-buffer-other-window buf)))
-
-(defun shell-other-frame (&optional buffer)
-  "Like `shell', but put buffer in another frame."
-  (interactive)
-  (let (buf (call-interactively 'shell))
-    (switch-to-buffer-other-frame buf)))
+             '("^\\*shell\\*" .
+               ((display-buffer-reuse-window display-buffer-same-window) (reusable-frames . t))))
 
 (global-set-key (kbd "C-x $") 'shell) ; original is set-selective-display
-(global-set-key (kbd "C-x 4 $") 'shell-other-window)
-(global-set-key (kbd "C-x 5 $") 'shell-other-frame)
