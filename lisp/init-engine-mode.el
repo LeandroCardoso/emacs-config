08095ef101190affe0fd52afae1679e08be06373
Leandro Cardoso <leandrocardoso@gmail.com>
2017-09-04 18:51:53 -0300

    engine: added wikipedia and wiktionary

--- lisp/init-engine-mode.el
+++ lisp/init-engine-mode.el
@@ -12,4 +12,12 @@
   (defengine msdn
     "https://social.msdn.microsoft.com/search/en-US/windows?query=%s"
     :keybinding "m")
+
+  (defengine wikipedia
+    "https://en.wikipedia.org/w/index.php?search=%s"
+    :keybinding "w")
+
+  (defengine wiktionary
+    "https://en.wiktionary.org/w/index.php?search=%s"
+    :keybinding "y")
   )
