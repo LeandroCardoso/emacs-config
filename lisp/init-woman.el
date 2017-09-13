1c4a985953bc7bdb8401a9f2a9c62aa39b1bcdc0
Leandro Cardoso <leandrocardoso@gmail.com>
2017-09-13 14:42:29 -0300

    woman: changed keybinding to C-x m

--- lisp/init-woman.el
+++ lisp/init-woman.el
@@ -3,4 +3,10 @@
   (setq woman-use-symbol-font t)
   (set-face-attribute 'woman-bold nil :inherit '(Man-overstrike))
   (set-face-attribute 'woman-italic nil :inherit '(Man-underline))
-  (global-set-key (kbd "C-c m") 'woman))
+
+  ;; unset compose-mail keys to use it with woman
+  (global-unset-key (kbd "C-x m"))   ;; compose-mail
+  (global-unset-key (kbd "C-x 4 m")) ;; compose-mail-other-window
+  (global-unset-key (kbd "C-x 5 m")) ;; compose-mail-other-frame
+
+  (global-set-key (kbd "C-x m") 'woman))
