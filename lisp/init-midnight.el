97f6d81e94c72ba1aeb7f6b2b22242f74c31b599
Leandro Cardoso <leandrocardoso@gmail.com>
2017-09-18 17:08:03 -0300

    midnight: fix quote in lambda func

--- lisp/init-midnight.el
+++ lisp/init-midnight.el
@@ -1,6 +1,6 @@
 (require 'midnight)
 
 (setq clean-buffer-list-delay-general 2)
-(mapc '(lambda (ARG) (add-to-list 'clean-buffer-list-kill-regexps ARG)) '("^\\*.*\\*$"))
+(mapc (lambda (ARG) (add-to-list 'clean-buffer-list-kill-regexps ARG)) #("^\\*.*\\*$"))
 
 (midnight-mode t)
