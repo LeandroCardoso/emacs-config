09dbca98c9c960bfa37b936c440eb5e29f9207d0
Leandro Cardoso <leandrocardoso@gmail.com>
2017-11-17 17:05:13 -0200

    nxml: Added nxml-where function

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -24,5 +24,32 @@ XMLLINT_INDENT of the current buffer.
     nil
     "--format" "--recover" "--nowarning" "-")))
 
+;; Original from https://www.emacswiki.org/emacs/NxmlMode
+(defun nxml-where ()
+  "Display the hierarchy of XML elements the point is on as a path."
+  (interactive)
+  (let ((path nil))
+    (save-excursion
+      (save-restriction
+        (widen)
+        (while (and (< (point-min) (point)) ;; Doesn't error if point is at beginning of buffer
+                    (condition-case nil
+                        (progn
+                          (nxml-backward-up-element) ; always returns nil
+                          t)
+                      (error nil)))
+          (setq path (cons (xmltok-start-tag-local-name) path)))
+        (if (called-interactively-p t)
+            (message "/%s" (mapconcat 'identity path "/"))
+          (format "/%s" (mapconcat 'identity path "/")))))))
+
+;; (defun nxml-enable-which-func ()
+;;   (when (derived-mode-p 'nxml-mode)
+;;     (which-function-mode t)
+;;     (setq which-func-mode t)
+;;     (add-hook 'which-func-functions 'nxml-where t t)))
+
+;; (add-hook 'find-file-hook 'nxml-enable-which-func t)
+
 (with-eval-after-load "nxml-mode"
   (define-key nxml-mode-map (kbd "C-c p") 'xml-pretty-print))
