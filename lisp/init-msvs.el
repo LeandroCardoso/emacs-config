e0fa2d6f66247927efbbb7ec264cb1d96ebb1194
Leandro Cardoso <leandrocardoso@gmail.com>
2018-02-02 17:05:18 -0200

    msvs: added more files to grep alias

--- lisp/init-msvs.el
+++ lisp/init-msvs.el
@@ -91,7 +91,8 @@ Versions supported are from Visual Studio 2005 (8.0) up to Visual Studio 2015 (1
 
   (with-eval-after-load "grep"
     ;; MSVS
-    (add-to-list 'grep-files-aliases '("msvs" . "*.sln *proj *proj.filters *.props *.targets")))
+    (add-to-list 'grep-files-aliases
+                 '("msvs" . "*.sln *proj *proj.filters *.props *.targets packages.config app.config")))
 
   ;; set compile-command
   (setq compile-command (msvs-compile-command))
