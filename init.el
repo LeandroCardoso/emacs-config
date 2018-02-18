b3a8725e0c51bdffd579d872f732a46135a1c517
Leandro Cardoso <leandrocardoso@gmail.com>
2018-02-17 22:58:55 -0200

    init: ignore files starting with dot when loading files from lisp/

--- init.el
+++ init.el
@@ -7,15 +7,15 @@
 
 (setq custom-file (concat user-emacs-directory "custom-variables.el"))
 
-;; Load all *.el files sorted by name at ~/.emacs.d/lisp. Sub-directories and files starting with
-;; underline are ignored. If a compiled elisp file exist and it is not outdated, then load it
-;; instead of the non-compiled one.
+;; Load all emacs list (*.el/*.elc) files sorted by name at ~/.emacs.d/lisp. Sub-directories and
+;; files starting with underline or dot are ignored. If a compiled elisp file exist and it is not
+;; outdated, then load it instead of the non-compiled one.
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (setq load-prefer-newer t)
 (mapc 'load (mapcar 'file-name-base
                     (directory-files (expand-file-name "lisp" user-emacs-directory)
                                      nil
-                                     "^[^_].*\\.el$")))
+                                     "^[^_\\.].*\\.el$")))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
