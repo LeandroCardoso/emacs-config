6d47f7a61bd959c4cac7a6426bd9bec4e42811aa
Leandro Cardoso <leandrocardoso@gmail.com>
2018-02-18 18:44:34 -0300

    powerline: disable hud

--- lisp/init-powerline.el
+++ lisp/init-powerline.el
@@ -1,5 +1,6 @@
 (when (require 'powerline nil t)
   (setq powerline-default-separator 'slant)
+  (setq powerline-display-hud nil)
   (setq powerline-gui-use-vcs-glyph t)
   (setq powerline-height 28)
   ;; TODO customize mode-line-format instead of use powerline-default-theme
