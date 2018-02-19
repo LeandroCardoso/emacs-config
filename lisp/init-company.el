7389415bb4a6bbc3706a4e51e8e7cec9f8194f0b
Leandro Cardoso <leandrocardoso@gmail.com>
2018-02-19 15:50:51 -0300

    company: enable company-tng

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -3,7 +3,7 @@
     (setq-local company-backends
                 '((company-etags company-dabbrev-code company-yasnippet company-keywords)
                   (company-dabbrev-code company-yasnippet company-keywords))))
-  
+
   (add-hook 'c-mode-common-hook 'c-common-set-company)
 
   (setq company-idle-delay 0.3)
@@ -11,6 +11,7 @@
   (setq company-search-regexp-function 'company-search-flex-regexp)
   (setq company-show-numbers t)
   (setq company-transformers '(company-sort-by-backend-importance))
+  (company-tng-configure-default)
 
   ;; company dabbrev
   (setq company-dabbrev-downcase nil)
@@ -20,9 +21,6 @@
 
   ;; company etags
   (setq company-etags-everywhere t)
-
-  ;; TODO add c++ keywords to company-keywords-alist
-  ;; (alignas alignof char16_t char32_t constexpr decltype noexcept nullptr static_assert thread_local)
 
   ;; company-ispell
   (defun toggle-company-ispell ()
@@ -38,15 +36,11 @@
   ;; keymap
   (define-key prog-mode-map (kbd "<tab>") 'company-indent-or-complete-common)
   (define-key text-mode-map (kbd "<tab>") 'company-indent-or-complete-common)
-  (define-key company-mode-map (kbd "<C-tab>") 'company-complete)
-  
+
   (define-key company-search-map (kbd "<escape>") 'company-search-abort)
   (define-key company-active-map (kbd "<escape>") 'company-abort)
-  (define-key company-active-map (kbd "<tab>") 'company-select-next)
-  (define-key company-active-map (kbd "<backtab>") 'company-select-previous)
-  (define-key company-active-map (kbd "<S-tab>") 'company-select-previous)
 
   (global-company-mode)
-  
+
   (when (require 'company-flx nil t)
     (company-flx-mode +1)))
