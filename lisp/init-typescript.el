baa0c70a57e690cf25f4449013b77421b7e3cf90
Leandro Cardoso <leandrocardoso@gmail.com>
2018-02-20 17:59:45 -0300

    added typescript

---
+++ lisp/init-typescript.el
@@ -0,0 +1,25 @@
+(when (require 'tide nil t)
+  (setq tide-completion-detailed t)
+  (setq tide-hl-identifier-idle-time 0.1)
+  (setq tide-imenu-flatten t)
+
+  (defun tide-setup-hook ()
+    (tide-setup)
+    (flycheck-mode)
+    (eldoc-mode)
+    (tide-hl-identifier-mode))
+
+  ;; formats the buffer before saving
+  (add-hook 'before-save-hook 'tide-format-before-save)
+  (add-hook 'typescript-mode-hook #'tide-setup-hook)
+
+  ;; TODO add support to lint
+
+  ;; Force bundled tide tsserver
+  (defun tide-force-bundled-tsserver ()
+    "Force use of bundled tide tsserver.
+See `tide-tsserver-executable'."
+    (interactive)
+    (setq tide-tsserver-executable
+          (car (directory-files-recursively (concat user-emacs-directory "elpa/") tide--tsserver))))
+  )
