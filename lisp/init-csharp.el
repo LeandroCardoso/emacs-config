1de96398d3c3179d676e722c945a40e35ffc58b5
Leandro Cardoso <leandrocardoso@gmail.com>
2018-02-28 16:30:22 -0300

    csharp: added flycheck

--- lisp/init-csharp.el
+++ lisp/init-csharp.el
@@ -2,14 +2,17 @@
 (when (require 'csharp-mode nil t)
   ;; omnisharp
   (when (require 'omnisharp nil t)
+    (setq omnisharp-imenu-support t)
     (setq omnisharp-server-executable-path (concat user-emacs-directory "omnisharp/OmniSharp.exe"))
-    (add-hook 'csharp-mode-hook 'omnisharp-mode)
+    
+    (add-hook 'csharp-mode-hook #'omnisharp-mode)
+
+    (with-eval-after-load "flycheck"
+      (add-hook 'csharp-mode-hook #'flycheck-mode))
 
     ;; omnisharp + company
     (with-eval-after-load "company"
       (add-to-list 'company-backends 'company-omnisharp)
-      ;; this value is currently bugged (setq omnisharp-company-match-type 'company-match-flx)
       ;; this value is slow (setq omnisharp-company-match-type 'company-match-server)
-      (setq omnisharp-imenu-support t))
-    )
-  )
+      )
+    ))
