609ca603579ad5079d8c380bffe038a66a3efc00
Leandro Cardoso <leandrocardoso@gmail.com>
2018-03-13 17:19:20 -0300

    flycheck: increased flycheck-idle-change-delay

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -3,13 +3,13 @@
   (setq flycheck-checker-error-threshold nil)
   (setq flycheck-completing-read-function 'ido-completing-read)
   (setq flycheck-global-modes '(c++-mode csharp-mode typescript-mode))
-  (setq flycheck-idle-change-delay 3)
+  (setq flycheck-idle-change-delay 5)
   (setq flycheck-mode-line-prefix nil)
   ;; TODO customize flycheck-mode-line. See flycheck-mode-line-status-text
 
   ;; cppcheck
   ;; unusedStructMember is annoying in header files
   (setq flycheck-cppcheck-suppressions '("unusedStructMember"))
-  
+
   (global-flycheck-mode)
   )
