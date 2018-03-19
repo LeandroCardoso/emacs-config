e8cac2e4d3b4abab8a43b918e813f6dae8f54af9
Leandro Cardoso <leandrocardoso@gmail.com>
2018-03-19 17:46:24 -0300

    avy: change keys and key bidings

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -1,6 +1,9 @@
 (when (require 'avy nil t)
   (setq avy-all-windows nil)
-  (global-set-key (kbd "C-M-;") 'avy-goto-word-or-subword-1)
-  (global-set-key (kbd "<C-dead-acute>") 'avy-goto-word-or-subword-1)
-  (global-set-key (kbd "C-M-:") 'avy-goto-char)
+  (setq avy-dispatch-alist nil)
+  (setq avy-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l ?\; ?q ?w ?e ?r ?t ?y ?u ?i ?o ?p ?z ?x ?c ?v ?b ?n ?m ?, ?. ?/))
+
+  (global-set-key (kbd "C-M-;") 'avy-goto-char)
+  (global-set-key (kbd "<C-dead-acute>") 'avy-goto-char)
+  (global-set-key (kbd "C-M-:") 'avy-goto-char-in-line)
   )
