bda377db1fa5bfcdd209ad6541743baa25ba62fd
Leandro Cardoso <leandrocardoso@gmail.com>
2018-04-04 17:21:49 -0300

    hl-line: Hack to disable global-hl-line-mode in ediff registry buffers

--- lisp/init-hl-line.el
+++ lisp/init-hl-line.el
@@ -3,3 +3,6 @@
 ;; global-hl-line-mode causes slowness when scrolling down repeatedly, this is a good workaround for
 ;; it.
 (setq auto-window-vscroll nil)
+
+;; Hack to disable global-hl-line-mode in ediff registry buffers
+(add-hook 'ediff-mode-hook '(lambda () (setq-local global-hl-line-mode nil)))
