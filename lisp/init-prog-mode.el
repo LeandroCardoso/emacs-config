a52366ca248ee61be74a36778956b13eebc74a27
Leandro Cardoso <leandrocardoso@gmail.com>
2018-04-25 16:13:15 -0300

    progmode: improved smart-semicolon and created electric-semicolon

--- lisp/init-prog-mode.el
+++ lisp/init-prog-mode.el
@@ -3,19 +3,30 @@
     (font-lock-add-keywords
      nil
      '(("\\<\\(FIXME\\|TODO\\|BUG\\)\\>" 1 font-lock-warning-face t))))
-  
+
   (add-hook 'prog-mode-hook #'font-lock-todo-setup))
 
 
 ;; functions
 
-(defun smart-dot-comma ()
-  "Go to end of line and insert a \";\""
+(defun smart-semicolon ()
+  "Go to end of line, delete trailing whitespace and insert a \";\" unless
+one already exists at point."
   (interactive "*")
   (move-end-of-line nil)
-  (insert ";"))
+  (delete-trailing-whitespace)
+  (unless (char-equal (preceding-char) (string-to-char ";"))
+    (insert ";")))
+
+(defun electric-semicolon ()
+  "Insert a \";\" unless one already exists at point, in this case forward one char."
+  (interactive "*")
+  (if (char-equal (following-char) (string-to-char ";"))
+      (forward-char)
+    (insert ";")))
 
 ;; key bindings
 
-(define-key prog-mode-map (kbd "C-;") 'smart-dot-comma)
+(define-key prog-mode-map (kbd ";") 'electric-semicolon)
+(define-key prog-mode-map (kbd "C-;") 'smart-semicolon)
 (define-key prog-mode-map (kbd "<f9>") 'compile)
