db59e504c1c6e42d1f7cb017b414689a3fa6b242
Leandro Cardoso <leandrocardoso@gmail.com>
2018-05-04 17:47:54 -0300

    desktop: removed old function and added desktop files to gitignore

--- lisp/init-desktop.el
+++ lisp/init-desktop.el
@@ -1,14 +1,3 @@
-(defun set-frame-title-from-desktop-dir ()
-  (setq frame-title-format
-        (list "emacs"
-              (when (boundp 'desktop-dirname)
-                (list " - " (file-name-nondirectory (directory-file-name desktop-dirname)))))))
-
 (with-eval-after-load "desktop"
-  (add-to-list 'desktop-path ".")
   (setq desktop-save 'ask-if-exists)
-
-  (desktop-save-mode)
-
-  (add-hook 'desktop-after-read-hook 'set-frame-title-from-desktop-dir)
-  (add-hook 'desktop-save-hook 'set-frame-title-from-desktop-dir))
+  (desktop-save-mode))
