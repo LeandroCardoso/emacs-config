4f45c9af430fbc859ca0f4542579701ff2115411
Leandro Cardoso <leandrocardoso@gmail.com>
2018-06-06 16:19:55 -0300

    grep: disable grep-save-buffers

--- lisp/init-grep.el
+++ lisp/init-grep.el
@@ -1,4 +1,6 @@
 (with-eval-after-load "grep"
+  (setq grep-save-buffers nil)
+
   ;; add *.c to c++ aliases
   (setcdr (assoc "cc" grep-files-aliases) "*.cc *.cxx *.cpp *.[Cc] *.CC *.c++")
   (setcdr (assoc "cchh" grep-files-aliases) "*.cc *.[ch]xx *.[ch]pp *.[CHch] *.CC *.HH *.[ch]++")
