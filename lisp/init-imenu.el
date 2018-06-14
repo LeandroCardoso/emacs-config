7465fc21415fc96403c0934f4fd855e3d3c1a1b8
Leandro Cardoso <leandrocardoso@gmail.com>
2018-06-14 17:18:56 -0300

    imenu: removed idomenu and add hooks to automaticly rescan

--- lisp/init-imenu.el
+++ lisp/init-imenu.el
@@ -1,10 +1,16 @@
-(with-eval-after-load "imenu"
+(when (require 'imenu nil t)
   (setq imenu-auto-rescan t)
   (setq imenu-auto-rescan-maxout 100000)
   (setq imenu-max-item-length nil)
 
-  (add-hook 'imenu-after-jump-hook 'reposition-window))
+  (add-hook 'imenu-after-jump-hook 'reposition-window)
 
-;; idomenu
-(when (require 'idomenu nil t)
-  (global-set-key (kbd "C-z") 'idomenu)) ; default is suspend-frame
+  (defun imenu-rescan ()
+    (interactive)
+    (imenu--menubar-select imenu--rescan-item))
+
+  (add-hook 'after-save-hook 'imenu-rescan)
+  (add-hook 'find-file-hook 'imenu-rescan)
+
+  (global-set-key (kbd "C-z") 'imenu) ; default is suspend-frame
+  )
