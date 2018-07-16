6d06ed15f326c67501846e8cf2c918288917f224
Leandro Cardoso <leandrocardoso@gmail.com>
2018-07-16 14:23:24 -0300

    recentf: use abbreviate-file-name

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -8,21 +8,24 @@
 
 This function works better with a `completing-read' enhancement like `ido'."
   (interactive)
-  (find-file (completing-read "Find recent file: " recentf-list nil t)))
+  (find-file (completing-read "Find recent file: "
+                              (mapcar #'abbreviate-file-name recentf-list) nil t)))
 
 (defun recentf-find-file-other-window ()
   "Edit file from the `recentf-list', in another window.
 
 This function works better with a `completing-read' enhancement like `ido'."
   (interactive)
-  (find-file-other-window (completing-read "Find recent file: " recentf-list nil t)))
+  (find-file-other-window (completing-read "Find recent file: "
+                                           (mapcar #'abbreviate-file-name recentf-list) nil t)))
 
 (defun recentf-find-file-other-frame ()
   "Edit file from the `recentf-list', in another frame.
 
 This function works better with a `completing-read' enhancement like `ido'."
   (interactive)
-  (find-file-other-frame (completing-read "Find recent file: " recentf-list nil t)))
+  (find-file-other-frame (completing-read "Find recent file: "
+                                          (mapcar #'abbreviate-file-name recentf-list) nil t)))
 
 (global-set-key (kbd "C-x C-r") 'recentf-find-file) ; default is find-file-read-only
 (global-set-key (kbd "C-x 4 C-r") 'recentf-find-file-other-window) ; default is find-file-read-only-other-window
