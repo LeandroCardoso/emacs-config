bcec72d9788edd294448e841f5c45eb3cfd9494d
Leandro Cardoso <leandrocardoso@gmail.com>
2018-07-16 14:22:52 -0300

    improved find-file-sudo

--- lisp/init-find-files.el
+++ lisp/init-find-files.el
@@ -1,11 +1,17 @@
 (setq ff-case-fold-search t)
 
 (unless (eq system-type 'windows-nt)
-  (defun find-file-sudo ()
-    "Reopen the current file as root, preserving point position."
-    (interactive)
-    (let ((p (point)))
-      (find-alternate-file (concat "/sudo::" buffer-file-name))
-      (goto-char p))))
+  (defun find-file-sudo (&optional arg)
+    "Reopen the current file as root, preserving point position.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current buffer is not visiting a file."
+    (interactive "P")
+    (if (or arg (not buffer-file-name))
+        (find-file (concat "/sudo:root@localhost:"
+                           (ido-read-file-name "Find file (as root): ")))
+      (let ((p (point)))
+        (find-alternate-file (concat "/sudo::root@localhost" buffer-file-name))
+        (goto-char p)))))
 
 (global-set-key (kbd "C-M-o") 'ff-find-other-file) ;; default is split-line
