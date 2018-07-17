59845855acb8f001561c11a3aa55e706ef1e74f5
Leandro Cardoso <leandrocardoso@gmail.com>
2018-07-17 16:21:35 -0300

    ibuffer: added ibuffer-set-filter-groups-by-project-root and set it by default

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -1,4 +1,20 @@
 (with-eval-after-load "ibuffer"
+
+  (require 'project)
+  (defun ibuffer-set-filter-groups-by-project-root ()
+    "Set the current filter groups to filter by project root directory."
+    (interactive)
+    (setq ibuffer-filter-groups
+          (mapcar (lambda (pr)
+                    (cons (format "%s" pr) `((filename . ,pr))))
+                  (delq nil (ibuffer-remove-duplicates
+                          (mapcar (lambda (buf)
+                                    (let ((pr (with-current-buffer buf (project-current))))
+                                      (when pr
+                                        (car (project-roots pr)))))
+                                  (buffer-list))))))
+    (ibuffer-update nil t))
+
   ;;(setq ibuffer-display-summary nil)
   (setq ibuffer-formats
         '((mark modified read-only " "
@@ -10,7 +26,8 @@
                 " " filename-and-process)
           (mark " " (name 40 -1) " " filename)))
 
-  (add-hook 'ibuffer-mode-hook (lambda () (ibuffer-auto-mode))))
+  (add-hook 'ibuffer-hook #'ibuffer-set-filter-groups-by-project-root)
+  (add-hook 'ibuffer-mode-hook #'ibuffer-auto-mode))
 
 (global-set-key (kbd "C-x C-b") 'ibuffer) ;; default is list-buffers
 (global-set-key (kbd "C-x 4 C-b") 'ibuffer-other-window)
