507f772554a05f47ac5596e26568799defbb5004
Leandro Cardoso <leandrocardoso@gmail.com>
2018-07-20 17:42:03 -0300

    ido: enable vertical mode, decrease ido-cr+-max-items and disable ido-show-dot-for-dired

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -2,7 +2,7 @@
 (ido-everywhere t)
 
 (when (require 'ido-completing-read+ nil t)
-  (setq ido-cr+-max-items 100000)
+  (setq ido-cr+-max-items 50000)
   (ido-ubiquitous-mode t))
 
 (when (require 'flx-ido nil t)
@@ -21,10 +21,14 @@
       '(" { " " }" " | " " | +" "" "" " [No match]" "" " [Not readable]" " [Too big]" " [Confirm]" " [" "]"))
 (setq ido-default-buffer-method 'selected-window)
 (setq ido-enable-flex-matching t)
-(setq ido-show-dot-for-dired t)
+;; (setq ido-show-dot-for-dired t)
 (setq ido-use-filename-at-point 'guess)
 (setq ido-use-url-at-point t)
 
 ;; Enable ido in dired commands
 (put 'dired-do-copy   'ido nil)
 (put 'dired-do-rename 'ido nil)
+
+(when (require 'ido-vertical-mode nil t)
+  (setq ido-vertical-show-count t)
+  (ido-vertical-mode))
