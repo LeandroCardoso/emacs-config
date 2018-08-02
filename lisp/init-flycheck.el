6293a9b8a847bbecd45ae630a231e05504836c3f
Leandro Cardoso <leandrocardoso@gmail.com>
2018-08-02 16:35:40 -0300

    flycheck: add c-mode

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,7 +2,7 @@
   (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled))
   (setq flycheck-checker-error-threshold nil)
   (setq flycheck-completing-read-function 'ido-completing-read)
-  (setq flycheck-global-modes '(c++-mode csharp-mode typescript-mode))
+  (setq flycheck-global-modes '(c-mode c++-mode csharp-mode typescript-mode))
   (setq flycheck-idle-change-delay 5)
   (setq flycheck-mode-line-prefix nil)
   ;; TODO customize flycheck-mode-line. See flycheck-mode-line-status-text
