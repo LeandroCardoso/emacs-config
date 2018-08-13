eeabb0f713f02ea339244466c4ef95f4ad2b25f9
Leandro Cardoso <leandrocardoso@gmail.com>
2018-08-13 16:33:55 -0300

    xml: add support to region in xml-pretty-print function

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -3,29 +3,28 @@
   (setq nxml-slash-auto-complete-flag t)
 
 
-  (defun xml-pretty-print ()
+  (defun xml-pretty-print (start end)
     "Simple-minded pretty printer for XML.
 Re-indents the XML and inserts newlines using xmllint (from
 libxml2) tool.
+
+If the region is active, act on region, otherwise act on the whole buffer.
 
 This function try to respect the `indent-tabs-mode' and
 `nxml-child-indent' variables and set the environment variable
 XMLLINT_INDENT of the current buffer.
 "
-    (interactive "*")
+    (interactive "*r")
     (if (executable-find "xmllint")
         (progn
           (make-local-variable 'process-environment)
           (setenv "XMLLINT_INDENT"
                   (if indent-tabs-mode (make-string 1 9) (make-string nxml-child-indent 32)))
           (save-mark-and-excursion
-           (call-process-region
-            (point-min) (point-max)
-            "xmllint"
-            t
-            '(t nil)
-            nil
-            "--format" "--recover" "--nowarning" "-")))
+            (let ((min (if mark-active start (point-min)))
+                  (max (if mark-active end (point-max))))
+              (call-process-region min max "xmllint" t '(t nil) nil
+                                   "--format" "--recover" "--nowarning" "-"))))
       (error "xmllint executable not found")))
 
   ;; Original from https://www.emacswiki.org/emacs/NxmlMode
