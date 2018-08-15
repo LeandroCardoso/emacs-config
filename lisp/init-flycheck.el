37eef1506e45935592be3500096827e53edebf13
Leandro Cardoso <leandrocardoso@gmail.com>
2018-08-15 09:36:24 -0300

    flycheck: set flycheck-mode-line-prefix

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -4,7 +4,7 @@
   (setq flycheck-completing-read-function 'ido-completing-read)
   (setq flycheck-global-modes '(c-mode c++-mode csharp-mode typescript-mode))
   (setq flycheck-idle-change-delay 5)
-  (setq flycheck-mode-line-prefix nil)
+  (setq flycheck-mode-line-prefix "!")
   ;; TODO customize flycheck-mode-line. See flycheck-mode-line-status-text
 
   ;; cppcheck
