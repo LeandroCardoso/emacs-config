ca12b63e5d44799e7f75ea996d944dda41ba5bab
Leandro Cardoso <leandrocardoso@gmail.com>
2018-08-23 16:20:43 -0300

    Redefine xref-backend-references to include "-s" parameter

--- lisp/init-gxref.el
+++ lisp/init-gxref.el
@@ -4,7 +4,7 @@
   ;; Each file whose suffix is `.h` is treated as a C++ source file.
   (setenv "GTAGSFORCECPP" "1")
 
-  ;; Redefine gxref--find-symbol without the call to shell-quote-argument to workaround in Windows.
+  ;; Redefine without the call to shell-quote-argument to workaround in Windows.
   (defun gxref--find-symbol (symbol &rest args)
     "Run GNU Global to find a symbol SYMBOL.
 Return the results as a list of xref location objects.  ARGS are
@@ -15,6 +15,10 @@ any additional command line arguments to pass to GNU Global."
            (global-output (gxref--global-to-list process-args)))
       (mapcar #'gxref--make-xref-from-gtags-x-line global-output)
       ))
+
+  ;; Redefine to include "-s" parameter
+  (cl-defmethod xref-backend-references ((_backend (eql gxref)) symbol)
+    (gxref--find-symbol symbol "-r" "-s"))
 
   (defun gxref-create-project-db ()
     "Create a GTAGS database in the first directory specified as `project-roots'"
