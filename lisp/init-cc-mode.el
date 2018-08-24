352c681fc9b211c5069639a5ae77d0725e058429
Leandro Cardoso <leandrocardoso@gmail.com>
2018-08-24 13:15:54 -0300

    cc: add new custom style for c and c++

--- lisp/init-cc-mode.el
+++ lisp/init-cc-mode.el
@@ -1,8 +1,10 @@
 (with-eval-after-load "cc-mode"
   (setq c-guess-region-max 100000)
 
-  (add-to-list 'c-default-style '(c++-mode . "stroustrup"))
-  (add-to-list 'c-default-style '(c-mode . "stroustrup"))
+  (c-add-style "c++-custom" '("stroustrup" (c-offsets-alist (inlambda . 0))))
+
+  (add-to-list 'c-default-style '(c++-mode . "c++-custom"))
+  (add-to-list 'c-default-style '(c-mode . "c++-custom"))
 
   ;; find-file
   (setq cc-search-directories '("." "./*" "../*" "/usr/include" "/usr/local/include/*"))
@@ -11,13 +13,14 @@
   (add-to-list 'c++-font-lock-extra-types "BOOL")
 
   ;; hooks
-  (defun c-common-setup-hook ()
-    (font-lock-add-keywords nil '(("\\<\\(TRUE\\|FALSE\\)\\>" . 'font-lock-constant-face))))
+  (defun c-common-setup-hook ())
 
   (defun c-setup-hook ()
+    (font-lock-add-keywords nil '(("\\<\\(TRUE\\|FALSE\\)\\>" . 'font-lock-constant-face)))
     (c-toggle-comment-style -1))
 
-  (defun c++-setup-hook ())
+  (defun c++-setup-hook ()
+    (font-lock-add-keywords nil '(("\\<\\(TRUE\\|FALSE\\)\\>" . 'font-lock-constant-face))))
 
   (add-hook 'c-mode-common-hook #'c-common-setup-hook)
   (add-hook 'c-mode-hook #'c-setup-hook)
