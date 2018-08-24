59d9a2c06d6cdc521ea96699cbdbddadc7924fa9
Leandro Cardoso <leandrocardoso@gmail.com>
2018-08-24 13:19:31 -0300

    avy: changed bidings

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -2,8 +2,9 @@
   (setq avy-all-windows nil)
   (setq avy-dispatch-alist nil)
   (setq avy-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l ?\; ?q ?w ?e ?r ?t ?y ?u ?i ?o ?p ?z ?x ?c ?v ?b ?n ?m ?, ?. ?/))
+  (setq avy-subword-extra-word-chars nil)
 
-  (global-set-key (kbd "C-M-;") 'avy-goto-word-or-subword-1)
-  (global-set-key (kbd "<C-dead-acute>") 'avy-goto-word-or-subword-1)
+  (global-set-key (kbd "C-M-;") 'avy-goto-word-1)
+  (global-set-key (kbd "<C-dead-acute>") 'avy-goto-word-1)
   (global-set-key (kbd "C-M-:") 'avy-goto-char-in-line)
   )
