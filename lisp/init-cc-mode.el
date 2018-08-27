0ec1f949dbda974c82ae3329a41b5928838ac9a9
Leandro Cardoso <leandrocardoso@gmail.com>
2018-08-27 14:19:56 -0300

    cc-mode: added interactive function for c-or-c++mode

--- lisp/init-cc-mode.el
+++ lisp/init-cc-mode.el
@@ -1,6 +1,12 @@
 (with-eval-after-load "cc-mode"
   (setq c-guess-region-max 100000)
 
+  (defun c-or-c++mode-i ()
+    "Analyze buffer and enable either C or C++ mode.
+This is the interactive version of `c-or-c++-mode'."
+    (interactive "P")
+    (c-or-c++-mode))
+
   (c-add-style "c++-custom" '("stroustrup" (c-offsets-alist (inlambda . 0))))
 
   (add-to-list 'c-default-style '(c++-mode . "c++-custom"))
