8a5e5edb7cb8effe191b2298378330f366034dff
Leandro Cardoso <leandrocardoso@gmail.com>
2018-09-14 15:20:26 -0300

    flycheck: disable mode-enable in Windows

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,11 +1,12 @@
 (when (require 'flycheck nil t)
-  (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled))
+  (setq flycheck-check-syntax-automatically (if (eq system-type 'windows-nt)
+                                                '(save idle-change)
+                                              '(save idle-change mode-enabled)))
   (setq flycheck-checker-error-threshold nil)
   (setq flycheck-completing-read-function 'ido-completing-read)
   (setq flycheck-global-modes '(c-mode c++-mode csharp-mode typescript-mode))
   (setq flycheck-idle-change-delay 5)
   (setq flycheck-mode-line-prefix "!")
-  ;; TODO customize flycheck-mode-line. See flycheck-mode-line-status-text
 
   ;; cppcheck
   ;; unusedStructMember is annoying in header files
