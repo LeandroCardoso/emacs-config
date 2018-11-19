1da3b6a7c35f22d172b4b2216288ed5ad48c6e9b
Leandro Cardoso <leandrocardoso@gmail.com>
2018-11-19 16:08:03 -0200

    midnight: don't cleanup the buffer list when I am idle for more than 1 day, like weekends and holidays

--- lisp/init-midnight.el
+++ lisp/init-midnight.el
@@ -2,4 +2,14 @@
 
 (setq clean-buffer-list-delay-general 1)
 
+(defun clean-buffer-list-check-idle-time ()
+  "Return t if emacs idle time is less than the
+`clean-buffer-list-delay-general'."
+  (let (idle (current-idle-time))
+    (or (not idle)
+        (< (round (float-time idle)) (* clean-buffer-list-delay-general 24 60 60)))))
+
+;; Don't cleanup the buffer list when I am idle for more than 1 day, like weekends and holidays.
+(advice-add 'clean-buffer-list :before-while #'clean-buffer-list-check-idle-time)
+
 (midnight-mode t)
