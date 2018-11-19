4bbb84626aaa6b31e57a7dff240163949bec3d7e
Leandro Cardoso <leandrocardoso@gmail.com>
2018-11-19 16:07:13 -0200

    added moody

---
+++ lisp/26-init-moody.el
@@ -0,0 +1,17 @@
+(when (require 'moody nil t)
+  ;; Make sure that the face `mode-line' does not set `:box' and that `:underline' and `:overline'
+  ;; are the same color or are both `undefined'. If defined, then the line color should be different
+  ;; from the `:background' colors of both `mode-line'and `default'. The same rules apply to
+  ;; `mode-line-inactive'. The line colors of `mode-line' and `mode-line-inactive' do not
+  ;; necessarily have to be identical.
+
+  (set-face-attribute 'mode-line          nil :overline   'unspecified)
+  (set-face-attribute 'mode-line          nil :underline  'unspecified)
+  (set-face-attribute 'mode-line          nil :box        'unspecified)
+  (set-face-attribute 'mode-line-inactive nil :overline   'unspecified)
+  (set-face-attribute 'mode-line-inactive nil :underline  'unspecified)
+  (set-face-attribute 'mode-line-inactive nil :box        'unspecified)
+  
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode))
