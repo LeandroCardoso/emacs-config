5861b07273ace6cec3cbb58aa635fed1ce27bd44
Leandro Cardoso <leandrocardoso@gmail.com>
2018-11-23 19:02:20 -0200

    changed whitespace-keymap

--- lisp/init-whitespace.el
+++ lisp/init-whitespace.el
@@ -1,15 +1,17 @@
 (when (require 'whitespace nil t)
   (setq whitespace-line-column nil)     ; use `fill-column'
+
   (defvar whitespace-keymap
     (let ((map (make-sparse-keymap)))
-      (define-key map "w" 'whitespace-mode)
+      (define-key map "C" 'whitespace-cleanup)
+      (define-key map "c" 'whitespace-cleanup-region)
       (define-key map "n" 'whitespace-newline-mode)
-      (define-key map "W" 'whitespace-toggle-options)
-      (define-key map "c" 'whitespace-cleanup)
-      (define-key map "t" 'delete-trailing-whitespace) ; simple.el
+      (define-key map "o" 'whitespace-toggle-options)
       (define-key map "r" 'delete-whitespace-rectangle) ; rect.el
+      (define-key map "t" 'delete-trailing-whitespace) ; simple.el
+      (define-key map "w" 'whitespace-mode)
       map)
     "Keymap for whitespace commands")
+
   (defalias 'whitespace-keymap whitespace-keymap)
-  (global-set-key (kbd "C-c w") 'whitespace-keymap)
-  )
+  (global-set-key (kbd "C-c w") 'whitespace-keymap))
