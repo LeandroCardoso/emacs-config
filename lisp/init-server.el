47237584698102dbbc2094dc3305a9430f7f5e6c
Leandro Cardoso <leandrocardoso@gmail.com>
2019-06-27 15:21:46 -0300

    do not start server unless noninteractive

--- lisp/init-server.el
+++ lisp/init-server.el
@@ -1,5 +1,5 @@
 ;; Start the emacs server needed by the emacsclient
 (require 'server)
-(unless (eq (server-running-p) t)
+(unless (and noninteractive (eq (server-running-p) t))
   (server-start)
   (message "Server started"))
