80d15a6d2c81aa1abd2f469da29407ca120366cd
Leandro Cardoso <leandrocardoso@gmail.com>
2019-07-03 14:22:59 -0300

    server: fix starting server

--- lisp/init-server.el
+++ lisp/init-server.el
@@ -1,5 +1,5 @@
 ;; Start the emacs server needed by the emacsclient
 (require 'server)
-(unless (and noninteractive (eq (server-running-p) t))
+(unless (or noninteractive (eq (server-running-p) t))
   (server-start)
   (message "Server started"))
