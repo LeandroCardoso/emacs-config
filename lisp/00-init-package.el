f67cd3f52404bce5535555613fdd66ab75d90dbd
Leandro Cardoso <leandrocardoso@gmail.com>
2019-08-02 14:10:50 -0300

    package: Use https urls, and add workaround for emacs 26.1 in Windows

--- lisp/00-init-package.el
+++ lisp/00-init-package.el
@@ -1,8 +1,12 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa stable" . "http://stable.melpa.org/packages/"))
+;; Workaround for emacs 26.1. See https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
+(when (eq system-type 'windows-nt)
+  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa stable" . "https://stable.melpa.org/packages/"))
 
 (setq package-archive-priorities
       '(("melpa"        . 2)
