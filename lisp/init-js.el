266a069c2ac90fe0dfebe3334617859ac3f8f04a
Leandro Cardoso <leandrocardoso@gmail.com>
2019-08-07 17:58:53 -0300

    js: use js2-minor-mode, instead of major-mode

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -1,5 +1,14 @@
 (when (require 'js2-mode nil t)
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  ;; Use js2-minor-mode for syntax highlight, jump-to-definition and imenu
+
+  (defun js2-minor-mode-setup ()
+    (setq-local imenu-create-index-function #'js2-mode-create-imenu-index))
+
+  (define-key js2-minor-mode-map [remap js-find-symbol] #'js2-jump-to-definition)
+
+  (add-hook 'js2-minor-mode-hook 'js2-minor-mode-setup)
+  (add-hook 'js-mode-hook 'js2-minor-mode)
+
   (setq js2-missing-semi-one-line-override t)
   (setq js2-mode-assume-strict t)
-  )
+  (setq-default js2-idle-timer-delay 1))
