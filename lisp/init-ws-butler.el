5361bbe97bbf5b4d1e3c3bdbd2993ef1c071acf0
Leandro Cardoso <leandrocardoso@gmail.com>
2019-10-21 17:20:59 -0300

    ws-butler: disable when merging in ediff

--- lisp/init-ws-butler.el
+++ lisp/init-ws-butler.el
@@ -1,3 +1,20 @@
 (when (require 'ws-butler nil t)
   (setq ws-butler-keep-whitespace-before-point nil)
+
+  (defun ws-butler-enable-ediff-buffer-C ()
+    (when (and ediff-buffer-C
+               ws-butler-global-mode
+               (not (apply (function derived-mode-p) ws-butler-global-exempt-modes)))
+      (with-current-buffer ediff-buffer-C
+        (ws-butler-mode))))
+
+  (defun ws-butler-disable-ediff-buffer-C ()
+    (when ediff-buffer-C
+      (with-current-buffer ediff-buffer-C
+        (ws-butler-mode -1))))
+
+  (add-hook 'ediff-startup-hook 'ws-butler-disable-ediff-buffer-C)
+  (add-hook 'ediff-cleanup-hook 'ws-butler-enable-ediff-buffer-C)
+  (add-hook 'ediff-suspend-hook 'ws-butler-enable-ediff-buffer-C)
+
   (ws-butler-global-mode))
