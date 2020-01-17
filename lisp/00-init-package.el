026f678bf4d7cca5055e8bc589f8c5e298c38312
Leandro Cardoso <leandrocardoso@gmail.com>
2020-01-17 16:59:11 -0300

    Removed workaround for emacs 26.1 in Windows

--- lisp/00-init-package.el
+++ lisp/00-init-package.el
@@ -1,10 +1,6 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-;; Workaround for emacs 26.1. See https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
-(when (eq system-type 'windows-nt)
-  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
-
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa stable" . "https://stable.melpa.org/packages/"))
 
