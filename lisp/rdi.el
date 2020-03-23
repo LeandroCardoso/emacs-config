9729c8f7dbb8a284385ea7c47772a6258a86750e
Leandro Cardoso <leandrocardoso@gmail.com>
2020-03-23 17:39:25 -0300

    rdi: fix jira url

--- lisp/rdi.el
+++ lisp/rdi.el
@@ -179,7 +179,7 @@
   ;; engine
   (when (require 'engine-mode nil t)
     (defengine jira-rdi
-      "https://jira.rdisoftware.com/secure/QuickSearch.jspa?searchString=%s"
+      "https://jira.rdisoftware.com/jira/secure/QuickSearch.jspa?searchString=%s"
       :keybinding "j")
     (defengine jira-mcd
       "https://us-jira.mcd.com/secure/QuickSearch.jspa?searchString=%s"
