e234567b1b88398d0bf55f7b72f832a9738b00c9
Leandro Cardoso <leandrocardoso@gmail.com>
2020-08-27 17:03:41 -0300

    rdi: improved support in linux

--- lisp/rdi.el
+++ lisp/rdi.el
@@ -1,101 +1,133 @@
-(defun np6-setup ()
-  (interactive)
-  (require 'project)
-  (add-to-list 'project-root-list "~/Documents/bugs/")
-  (add-to-list 'project-root-list "~/Documents/env/")
+(require 'project)
+(add-to-list 'project-root-list "~/Documents/bugs/")
+(add-to-list 'project-root-list "~/Documents/env/")
+(add-to-list 'project-root-list "~/rdi/bugs/")
+(add-to-list 'project-root-list "~/rdi/env/")
 
-  ;; nps
-  (when (featurep 'js2-mode)
-    (defun nps-setup-hook ()
-      (when (string-match-p "\\.nps\\'" (or buffer-file-name ""))
-        (setq-local js2-highlight-external-variables nil)
-        (setq-local js2-include-browser-externs nil)
-        (setq-local js2-language-version 180)))
+;; nps
+(when (featurep 'js2-mode)
+  (defun nps-setup-hook ()
+    (when (string-match-p "\\.nps\\'" (or buffer-file-name ""))
+      (setq-local js2-highlight-external-variables nil)
+      (setq-local js2-include-browser-externs nil)
+      (setq-local js2-language-version 180)))
 
-    (add-hook 'js2-mode-hook #'nps-setup-hook)
-    (add-hook 'js2-minor-mode-hook #'nps-setup-hook))
+  (add-hook 'js2-mode-hook #'nps-setup-hook)
+  (add-hook 'js2-minor-mode-hook #'nps-setup-hook))
 
-  (add-to-list 'auto-mode-alist '("\\.nps\\'" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.nps\\'" . js-mode))
 
-  ;; There are some c++ files using .c extension.
-  (add-to-list 'auto-mode-alist '("\\.c\\'" . c-or-c++-mode))
+;; There are some c++ files using .c extension.
+(add-to-list 'auto-mode-alist '("\\.c\\'" . c-or-c++-mode))
 
-  (add-to-list 'auto-mode-alist '("product\\.specification" . nxml-mode))
-  (add-to-list 'auto-mode-alist '("STLD\\.raw" . nxml-mode))
+(add-to-list 'auto-mode-alist '("product\\.specification" . nxml-mode))
+(add-to-list 'auto-mode-alist '("STLD\\.raw" . nxml-mode))
 
-  ;; Force coding system in log files
-  (modify-coding-system-alist 'file "\\.log\\'" 'prefer-utf-8-dos)
+;; Force coding system in log files
+(modify-coding-system-alist 'file "\\.log\\'" 'prefer-utf-8-dos)
 
-  ;; np6 log mode
-  (define-generic-mode np6-log-mode ;; MODE
-    nil                             ;; COMMENT-LIST
-    nil                             ;; KEYWORD-LIST
-    '(("^\\<\\(INFO\\|DEBUG\\)\\>" . font-lock-function-name-face)
-      ("^\\<WARNING\\>" . compilation-warning-face)
-      ("^\\<\\(ERROR\\|FATAL\\)\\>" . compilation-error-face)
-      ("\t.*\t" . font-lock-comment-face)
-      ("Legacy [a-zA-Z]+ Log" . font-lock-comment-face)
-      )                                                ;; FONT-LOCK-LIST
-    '("[0-9]\\{8\\}\\(_DEBUG\\)?-[0-9]\\{3\\}\\.log$") ;; AUTO-MODE-LIST
-    (list
-     (lambda ()
-       (setq global-auto-revert-ignore-buffer t))
-     ) ;; FUNCTION-LIST
-    )
+;; np6 log mode
+(define-generic-mode np6-log-mode ;; MODE
+  nil                             ;; COMMENT-LIST
+  nil                             ;; KEYWORD-LIST
+  '(("^\\<\\(INFO\\|DEBUG\\)\\>" . font-lock-function-name-face)
+    ("^\\<WARNING\\>" . compilation-warning-face)
+    ("^\\<\\(ERROR\\|FATAL\\)\\>" . compilation-error-face)
+    ("\t.*\t" . font-lock-comment-face)
+    ("Legacy [a-zA-Z]+ Log" . font-lock-comment-face)
+    )                                                ;; FONT-LOCK-LIST
+  '("[0-9]\\{8\\}\\(_DEBUG\\)?-[0-9]\\{3\\}\\.log$") ;; AUTO-MODE-LIST
+  (list
+   (lambda ()
+     (setq global-auto-revert-ignore-buffer t))
+   ) ;; FUNCTION-LIST
+  )
 
-  ;; Ugly hack to disable automatic string highlight. This is disabled due to several malformed
-  ;; strings.
-  (require 'mode-local)
-  (setq-mode-local np6-log-mode font-lock-keywords-only t)
+;; Ugly hack to disable automatic string highlight. This is disabled due to several malformed
+;; strings.
+(require 'mode-local)
+(setq-mode-local np6-log-mode font-lock-keywords-only t)
 
-  ;; np6 production log mode
-  (define-generic-mode np6-prod-log-mode ;; MODE
-    nil                                  ;; COMMENT-LIST
-    nil                                  ;; KEYWORD-LIST
-    '(("\\(INFO\\|FINEST\\|DEBUG\\)" . font-lock-function-name-face)
-      ("WARNING" . compilation-warning-face)
-      ("\\(ERROR\\|FATAL\\)" . compilation-error-face)
-      ("^[0-9- :]*;" . font-lock-comment-face) ; timestamp
-      ("\\[com.*\\] Thread: .*$" . font-lock-comment-face)
-      )                                                  ;; FONT-LOCK-LIST
-    '("\\(newposv6\\|np6[a-z]*\\)-[0-9]\\.[0-9]\\.log$") ;; AUTO-MODE-LIST
-    (list
-     (lambda ()
-       (setq global-auto-revert-ignore-buffer t))
-     ) ;; FUNCTION-LIST
-    )
+;; np6 production log mode
+(define-generic-mode np6-prod-log-mode ;; MODE
+  nil                                  ;; COMMENT-LIST
+  nil                                  ;; KEYWORD-LIST
+  '(("\\(INFO\\|FINEST\\|DEBUG\\)" . font-lock-function-name-face)
+    ("WARNING" . compilation-warning-face)
+    ("\\(ERROR\\|FATAL\\)" . compilation-error-face)
+    ("^[0-9- :]*;" . font-lock-comment-face) ; timestamp
+    ("\\[com.*\\] Thread: .*$" . font-lock-comment-face)
+    )                                                  ;; FONT-LOCK-LIST
+  '("\\(newposv6\\|np6[a-z]*\\)-[0-9]\\.[0-9]\\.log$") ;; AUTO-MODE-LIST
+  (list
+   (lambda ()
+     (setq global-auto-revert-ignore-buffer t))
+   ) ;; FUNCTION-LIST
+  )
 
-  ;; np6 kiosk log mode
-  (define-generic-mode np6-kiosk-log-mode ;; MODE
-    nil                                   ;; COMMENT-LIST
-    nil                                   ;; KEYWORD-LIST
-    '((" \\(INFO\\|DEBUG\\) " . font-lock-function-name-face)
-      (" WARN " . compilation-warning-face)
-      (" ERROR " . compilation-error-face)
-      ("^[0-9]* [0-9]*\\.[0-9]* \\[[0-9 ]*\\]" . font-lock-comment-face)
-      )                                                        ;; FONT-LOCK-LIST
-    '("\\(Debug\\|Error\\|Info\\|Root\\.All\\|Warn\\)\\.log$") ;; AUTO-MODE-LIST
-    (list
-     (lambda ()
-       (setq global-auto-revert-ignore-buffer t))
-     ) ;; FUNCTION-LIST
-    )
-  (setq-mode-local np6-kiosk-log-mode font-lock-keywords-only t)
+;; np6 kiosk log mode
+(define-generic-mode np6-kiosk-log-mode ;; MODE
+  nil                                   ;; COMMENT-LIST
+  nil                                   ;; KEYWORD-LIST
+  '((" \\(INFO\\|DEBUG\\) " . font-lock-function-name-face)
+    (" WARN " . compilation-warning-face)
+    (" ERROR " . compilation-error-face)
+    ("^[0-9]* [0-9]*\\.[0-9]* \\[[0-9 ]*\\]" . font-lock-comment-face)
+    )                                                        ;; FONT-LOCK-LIST
+  '("\\(Debug\\|Error\\|Info\\|Root\\.All\\|Warn\\)\\.log$") ;; AUTO-MODE-LIST
+  (list
+   (lambda ()
+     (setq global-auto-revert-ignore-buffer t))
+   ) ;; FUNCTION-LIST
+  )
+(setq-mode-local np6-kiosk-log-mode font-lock-keywords-only t)
 
-  ;; .np6 and .npsharp mode
-  (define-generic-mode np6-mode        ;; MODE
-    '(";")                             ;; COMMENT-LIST
-    '("|")                             ;; KEYWORD-LIST
-    nil                                ;; FONT-LOCK-LIST
-    '("start.*\\.\\(np6\\|npsharp\\)") ;; AUTO-MODE-LIST
-    nil                                ;; FUNCTION-LIST
-    )
+;; .np6 and .npsharp mode
+(define-generic-mode np6-mode        ;; MODE
+  '(";")                             ;; COMMENT-LIST
+  '("|")                             ;; KEYWORD-LIST
+  nil                                ;; FONT-LOCK-LIST
+  '("start.*\\.\\(np6\\|npsharp\\)") ;; AUTO-MODE-LIST
+  nil                                ;; FUNCTION-LIST
+  )
 
-  (defconst np6-base-path "~/Documents/env/" "Initial path used by `np6-config'")
-  (defconst np6-plugins-src-path "c:/Dev/NpSharpRoot/Plugins/" "Source code path for np# plugins")
-  (defconst np6-core-src-path (cond ((file-exists-p "c:/Dev/np61/") "c:/Dev/np61/")
+;; engine
+(when (require 'engine-mode nil t)
+  (defengine jira-rdi
+    "https://jira.rdisoftware.com/jira/secure/QuickSearch.jspa?searchString=%s"
+    :keybinding "j")
+  (defengine jira-mcd
+    "https://us-jira.mcd.com/secure/QuickSearch.jspa?searchString=%s"
+    :keybinding "J"))
+
+;; flycheck-clang
+(when (require 'flycheck nil t)
+  (setq flycheck-clang-ms-extensions t)
+  (setq flycheck-clang-warnings '("all" "extra" "no-invalid-token-paste"))
+
+  (setq flycheck-clang-definitions nil)
+  (dolist (def '("_MSC_VER=1800" ;used by windows
+                 "_M_IX86"
+                 "_WIN32"
+                 "NPMODDEF" ;used by np61
+                 "XP_WIN"   ;used by js180
+                 ))
+    (push def flycheck-clang-definitions)))
+
+(defconst np6-plugins-src-path "c:/Dev/NpSharpRoot/Plugins/" "Source code path for np# plugins")
+(defconst np6-core-src-path (cond ((file-exists-p "c:/Dev/np61/") "c:/Dev/np61/")
                                     ((file-exists-p "~/dev/rdi/np61/") "~/dev/rdi/np61/")
                                     (t nil)) "Source code path for np61 core")
+
+(defun np6-plugin-name (&optional path)
+  (let ((path (or path default-directory)))
+    (when (and np6-plugins-src-path
+               (string-match (concat np6-plugins-src-path "\\([^/]+\\)") path))
+      (match-string-no-properties 1 path))))
+
+;; Environment setup
+(when (eq system-type 'windows-nt)
+  (defconst np6-base-path "~/Documents/env/" "Initial path used by `np6-config'")
   (defvar np6-path nil "Path for NP6 environment")
   (defvar np6-core-dest nil
     "Map of destination names to destination paths to copy np61 core binaries")
@@ -104,11 +136,10 @@
   (defun np6-config ()
     (interactive)
     (when (or (called-interactively-p)
-              (not np6-path)
-              (not np6-core-dest))
+              (not np6-path))
       (setq np6-path (read-directory-name "NP6 environment directory: " np6-base-path nil t))
       (setq np6-core-dest nil)
-      (dolist (dest '(("np61" . "bin")
+      (dolist (dest '(("np61" . "bin") ;TODO unify np61 and poscore
                       ("posCore" . "NpSharpBin/Plugins/Np6PosCore")
                       ("wayCore" . "NpSharpBin/Plugins/Np6WayCore")
                       ("sale" . "NpSharpBin/Plugins/Sale/accountingServiceBin")))
@@ -131,12 +162,6 @@
         (start-process cmd "*np6*" full-cmd)
         (view-buffer "*np6*"))))
 
-  (defun np6-plugin-name (&optional path)
-    (let ((path (or path default-directory)))
-      (when (and np6-plugins-src-path
-                 (string-match (concat np6-plugins-src-path "\\([^/]+\\)") path))
-        (match-string-no-properties 1 path))))
-
   (defun np6-plugin-copy-bin (&optional force)
     (interactive "P")
     (let ((plugin-name (np6-plugin-name)))
@@ -147,7 +172,7 @@
                                       (if np6-debug "/bin/Debug" "/bin/Release"))
                               (concat np6-path "NpSharpBin/Plugins/" plugin-name)
                               force))
-      (error "Plugin not found"))))
+        (error "Plugin not found"))))
 
   (defun np6-core-copy-bin (&optional force)
     (interactive "P")
@@ -180,75 +205,52 @@
           (define-key map "C" 'np6-core-copy-bin)
           map))
   (defalias 'np6-keymap np6-keymap)
-  (global-set-key (kbd "<f5>") 'np6-keymap)
+  (global-set-key (kbd "<f5>") 'np6-keymap))
 
-  ;; engine
-  (when (require 'engine-mode nil t)
-    (defengine jira-rdi
-      "https://jira.rdisoftware.com/jira/secure/QuickSearch.jspa?searchString=%s"
-      :keybinding "j")
-    (defengine jira-mcd
-      "https://us-jira.mcd.com/secure/QuickSearch.jspa?searchString=%s"
-      :keybinding "J"))
 
-  ;; flycheck-clang
-  (when (require 'flycheck nil t)
-    (setq flycheck-clang-ms-extensions t)
-    (setq flycheck-clang-warnings '("all" "extra" "no-invalid-token-paste"))
+;; np61 include path list
+(defvar np61-include-path-list nil
+  "A list of include paths for np61 project.")
 
-    (setq flycheck-clang-definitions nil)
-    (dolist (def '("_MSC_VER=1800" ;used by windows
-                   "_M_IX86"
-                   "_WIN32"
-                   "NPMODDEF" ;used by np61
-                   "XP_WIN"   ;used by js180
-                   ))
-      (push def flycheck-clang-definitions))
+(defun np61-update-include-path-list ()
+  "Update the `np61-include-path-list'."
+  (interactive)
+  (if np6-core-src-path
+      (progn
+        (setq np61-include-path-list nil)
+        (message "Updating np61-update-include-path-list...")
+        (let ((start-time (current-time))
+              (pr np6-core-src-path))
+          (dolist (path (nconc (directory-list (concat pr "src/"))
+                               (directory-list (concat pr "extSrc/"))))
+            ;; Skip directories that do not have header files
+            (when (directory-files path nil "\\.h.*" t)
+              (push path np61-include-path-list)))
+          (message "Updating np61-update-include-path-list...done in %g seconds"
+                   (float-time (time-since start-time)))))
+    (error "Np6 core not found")))
 
-    (defvar np61-include-path-list nil
-      "A list of include paths for np61 project.")
+(defun np61-c-c++-setup ()
+  "Set `flycheck-clang-include-path',
+`company-clang-arguments' nad `company-c-headers-path-user' with
+np61 and compiler directories."
+  (interactive)
+  (when (string= np6-core-src-path (car (project-roots (project-current))))
+    ;; update np61-include-path-list
+    (when (null np61-include-path-list) (np61-update-include-path-list))
+    ;; update flycheck-clang-include-path
+    (setq-local flycheck-clang-include-path np61-include-path-list)
+    (when msvs-include-directory (push msvs-include-directory flycheck-clang-include-path))
+    (when msvs-platform-sdk (push msvs-platform-sdk flycheck-clang-include-path))
+    ;; update company-clang-arguments
+    (setq-local company-clang-arguments
+                (mapcar* (lambda (path) (concat "-I" path)) np61-include-path-list))
+    (when msvs-include-directory (push (concat "-I" msvs-include-directory)
+                                       company-clang-arguments))
+    (when msvs-platform-sdk (push (concat "-I" msvs-platform-sdk) company-clang-arguments))
+    ;; update company-c-headers
+    (setq-local company-c-headers-path-user np61-include-path-list))
+  nil)
 
-    (defun np61-update-include-path-list ()
-      "Update the `np61-include-path-list'."
-      (interactive)
-      (if np6-core-src-path
-          (progn
-            (setq np61-include-path-list nil)
-            (message "Updating np61-update-include-path-list...")
-            (let ((start-time (current-time))
-                  (pr np6-core-src-path))
-              (dolist (path (nconc (directory-list (concat pr "src/"))
-                                   (directory-list (concat pr "extSrc/"))))
-                ;; Skip directories that do not have header files
-                (when (directory-files path nil "\\.h.*" t)
-                  (push path np61-include-path-list)))
-              (message "Updating np61-update-include-path-list...done in %g seconds"
-                       (float-time (time-since start-time)))))
-        (error "Np6 core not found")))
-
-    (defun np61-c-c++-setup ()
-      "Set `flycheck-clang-include-path' and
-`company-clang-arguments' with np61 and compiler directories."
-      (interactive)
-      (when (string= np6-core-src-path (car (project-roots (project-current))))
-        ;; update np61-include-path-list
-        (when (null np61-include-path-list) (np61-update-include-path-list))
-        ;; update flycheck-clang-include-path
-        (setq-local flycheck-clang-include-path np61-include-path-list)
-        (when msvs-include-directory (push msvs-include-directory flycheck-clang-include-path))
-        (when msvs-platform-sdk (push msvs-platform-sdk flycheck-clang-include-path))
-        ;; update company-clang-arguments
-        (setq-local company-clang-arguments
-                    (mapcar* (lambda (path) (concat "-I" path)) np61-include-path-list))
-        (when msvs-include-directory (push (concat "-I" msvs-include-directory)
-                                           company-clang-arguments))
-        (when msvs-platform-sdk (push (concat "-I" msvs-platform-sdk) company-clang-arguments))
-        ;; update company-c-headers
-        (setq-local company-c-headers-path-user np61-include-path-list))
-      nil)
-
-    (add-hook 'c-mode-hook 'np61-c-c++-setup)
-    (add-hook 'c++-mode-hook 'np61-c-c++-setup)))
-
-(when (string= (getenv "USERDOMAIN") "RDISOFTWARE")
-  (np6-setup))
+(add-hook 'c-mode-hook 'np61-c-c++-setup)
+(add-hook 'c++-mode-hook 'np61-c-c++-setup))
