b4ac21033a687845ed4cdf5be8f212566c37c4a4
Leandro Cardoso <leandrocardoso@gmail.com>
2020-09-15 22:38:49 -0300

    solarized: change theme customization to use custom-theme-set-faces

--- lisp/25-init-solarized.el
+++ lisp/25-init-solarized.el
@@ -1,37 +1,34 @@
 (when (and (require 'solarized-theme nil t) (display-graphic-p))
+  (require 'solarized-palettes)
   (setq solarized-distinct-doc-face t)
   (setq solarized-scale-org-headlines nil)
   (setq solarized-use-more-italic t)
   (setq solarized-use-variable-pitch nil)
 
+  (setq solarized-custom-faces
+        '("My personal solarized theme customization."
+          (custom-theme-set-faces
+           theme-name
+           `(cursor ((,class (:background ,blue))))
+           `(fringe ((,class (:foreground ,s-line))))
+           `(header-line ((,class (:foreground ,yellow :underline ,base02 :weight bold))))
+           `(minibuffer-prompt ((,class (:foreground ,yellow))))
+           `(mode-line ((,class (:background ,(solarized-color-blend base02 blue 0.85)))))
+           `(mode-line-buffer-id ((,class (:foreground ,yellow :weight bold))))
+           `(mode-line-inactive ((,class (:background ,base02))))
+           ;; dired
+           `(dired-header ((,class (:foreground ,yellow :underline t :weight bold))))
+           ;; transient
+           `(transient-separator ((,class (:foreground ,s-line))))
+           ;; symbol-overlay
+           `(symbol-overlay-default-face ((,class :inherit unspecified :foreground ,magenta)))
+           ;; woman
+           `(woman-bold ((,class (:inherit Man-overstrike))))
+           `(woman-italic ((,class (:inherit Man-underline)))))
+          ;; (custom-theme-set-variables theme-name)
+          ))
+
   (load-theme 'solarized-dark t)
 
-  (solarized-with-color-variables 'dark 'solarized-dark solarized-dark-color-palette-alist
-    '((set-face-background 'cursor blue)
-      (set-face-attribute 'header-line nil
-                          :foreground yellow
-                          :weight 'bold)
-      (set-face-foreground 'fringe (face-foreground 'vertical-border))
-      (set-face-foreground 'minibuffer-prompt yellow)
-      (set-face-foreground 'mode-line-buffer-id yellow)
-      (set-face-background 'mode-line (solarized-color-blend base02 blue 0.85))
-      (set-face-background 'mode-line-inactive base02)))
-
-  (with-eval-after-load "dired"
-    (set-face-attribute 'dired-header nil
-                        :foreground (face-foreground 'header-line)
-                        :background 'unspecified
-                        :weight 'bold))
-
-  (with-eval-after-load "transient"
-    (set-face-background 'transient-separator (face-foreground 'vertical-border)))
-
-  (with-eval-after-load "symbol-overlay"
-    (set-face-attribute 'symbol-overlay-default-face nil
-                        :inherit 'unspecified
-                        :foreground (face-background 'isearch))) ; magenta
-
-  (with-eval-after-load "woman"
-    (set-face-attribute 'woman-bold nil :inherit '(Man-overstrike))
-    (set-face-attribute 'woman-italic nil :inherit '(Man-underline)))
-  )
+  (solarized-with-color-variables
+    'dark 'solarized-dark solarized-dark-color-palette-alist solarized-custom-faces))
