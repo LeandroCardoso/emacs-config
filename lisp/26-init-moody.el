a626e34acb876a28ec4a43fddb344fb23d4afe79
Leandro Cardoso <leandrocardoso@gmail.com>
2020-09-16 16:49:41 -0300

    moody: increase moody-mode-line-height

--- lisp/26-init-moody.el
+++ lisp/26-init-moody.el
@@ -4,14 +4,14 @@
   ;; from the `:background' colors of both `mode-line'and `default'. The same rules apply to
   ;; `mode-line-inactive'. The line colors of `mode-line' and `mode-line-inactive' do not
   ;; necessarily have to be identical.
-
   (set-face-attribute 'mode-line          nil :overline   'unspecified)
   (set-face-attribute 'mode-line          nil :underline  'unspecified)
   (set-face-attribute 'mode-line          nil :box        'unspecified)
   (set-face-attribute 'mode-line-inactive nil :overline   'unspecified)
   (set-face-attribute 'mode-line-inactive nil :underline  'unspecified)
   (set-face-attribute 'mode-line-inactive nil :box        'unspecified)
-  
+
   (setq x-underline-at-descent-line t)
+  (setq moody-mode-line-height 30)
   (moody-replace-mode-line-buffer-identification)
   (moody-replace-vc-mode))
