972535ad8f62308215a1c51c2a799176427834b4
Leandro Cardoso <leandrocardoso@gmail.com>
2020-09-22 00:23:12 -0300

    project: added project-simple-root-list

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -1,22 +1,39 @@
 (with-eval-after-load 'project
-  (defvar project-root-list nil
-    "List of directories, where each directory is the root of some project.
-All subdirectories of a root project are considered a part of one project.")
+  (defcustom project-simple-root-list nil
+    "List of directories root projects.")
 
-  (defun project-try-root-dir (dir)
-    (let ((root (seq-find #'(lambda (pr)
-                              (and (> (length dir) (length pr))
-                                   (string-prefix-p pr dir)))
-                          (mapcar 'file-name-as-directory
-                                  (mapcar 'expand-file-name project-root-list)))))
+  (defcustom project-root-list nil
+    "List of directories where every subdirectory is a root project.")
+
+  (defun project-try-custom-root (filename)
+    (let* ((exfilename (expand-file-name filename))
+           (dir-regexp-list
+            (nconc
+             (mapcar #'(lambda (custom-root-dir)
+                         (concat "^\\("
+                                 (regexp-quote (file-name-as-directory (expand-file-name custom-root-dir)))
+                                 "[^/]*/\\)"))
+                     project-root-list)
+             (mapcar #'(lambda (custom-root-dir)
+                         (concat "^\\("
+                                 (regexp-quote (file-name-as-directory (expand-file-name custom-root-dir)))
+                                 "\\)"))
+                     project-simple-root-list)))
+           (root (seq-some #'(lambda (dir-regexp)
+                               (when (string-match dir-regexp exfilename)
+                                 (match-string 1 exfilename)))
+                           dir-regexp-list)))
       (when root
-        (cons 'root-dir
-              (abbreviate-file-name (substring dir 0 (string-match "/" dir (length root))))))))
+        (cons 'dynamic (abbreviate-file-name root)))))
 
-  (cl-defmethod project-roots ((project (head root-dir)))
+  (cl-defmethod project-roots ((project (head dynamic)))
     (list (cdr project)))
 
-  (add-to-list 'project-find-functions 'project-try-root-dir)
+  (add-to-list 'project-find-functions 'project-try-custom-root)
+
+  ;; Add emacs lisp to its own project by default. Currently there is not way to get the emacs lisp
+  ;; directory directly, so I am using a random library from emacs lisp directory.
+  (add-to-list 'project-simple-root-list (file-name-directory (find-library-name "misc")))
 
   (global-set-key (kbd "C-x C-M-f") 'project-find-file)
   (global-set-key (kbd "C-x C-M-g") 'project-find-regexp)
