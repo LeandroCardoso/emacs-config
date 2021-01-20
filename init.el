bea1707d6f34dc95a713bf5af21275c215f45383
Leandro Cardoso <leandrocardoso@gmail.com>
2021-01-19 21:45:22 -0300

    Byte-compile lisp packages on startup. Small refactoring

--- init.el
+++ init.el
@@ -2,8 +2,9 @@
 (setq gc-cons-threshold (* 32 1024 1024))
 
 ;; local packages
-(add-to-list 'load-path (expand-file-name "packages" user-emacs-directory))
-(byte-recompile-directory (expand-file-name "packages" user-emacs-directory) 0)
+(let ((local-packages (expand-file-name "packages" user-emacs-directory)))
+  (add-to-list 'load-path local-packages)
+  (byte-recompile-directory local-packages 0))
 
 (when (< emacs-major-version 27)
   (package-initialize))
@@ -11,12 +12,11 @@
 ;; Load all emacs list (*.el/*.elc) files sorted by name at ~/.emacs.d/lisp. Sub-directories and
 ;; files starting with underline or dot are ignored. If a compiled elisp file exist and it is not
 ;; outdated, then load it instead of the non-compiled one.
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (setq load-prefer-newer t)
-(mapc 'load (mapcar 'file-name-base
-                    (directory-files (expand-file-name "lisp" user-emacs-directory)
-                                     nil
-                                     "^[^_\\.].*\\.el$")))
+(let ((local-lisp (expand-file-name "lisp" user-emacs-directory)))
+  (add-to-list 'load-path local-lisp)
+  (byte-recompile-directory local-lisp 0)
+  (mapc 'load (mapcar 'file-name-base (directory-files local-lisp nil "^[^_\\.].*\\.el$"))))
 
 (setq gc-cons-threshold (car (get 'gc-cons-threshold 'standard-value)))
 (message "emacs started in %s" (emacs-init-time))
