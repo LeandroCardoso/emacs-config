a6ab163a57e3ac7743d894a068fcd388db971036
Leandro Cardoso <leandrocardoso@gmail.com>
2021-01-21 22:39:27 -0300

    solarized: changed modeline, small improvements

--- lisp/25-init-solarized.el
+++ lisp/25-init-solarized.el
@@ -5,30 +5,31 @@
   (setq solarized-use-more-italic t)
   (setq solarized-use-variable-pitch nil)
 
-  (setq solarized-custom-faces
-        '("My personal solarized theme customization."
-          (custom-theme-set-faces
-           theme-name
-           `(cursor ((,class (:background ,blue))))
-           `(fringe ((,class (:foreground ,s-line))))
-           `(header-line ((,class (:foreground ,yellow :underline ,base02 :weight bold))))
-           `(minibuffer-prompt ((,class (:foreground ,yellow))))
-           `(mode-line ((,class (:background ,(solarized-color-blend base02 blue 0.85)))))
-           `(mode-line-buffer-id ((,class (:foreground ,yellow :weight bold))))
-           `(mode-line-inactive ((,class (:background ,base02))))
-           ;; dired
-           `(dired-header ((,class (:foreground ,yellow :underline t :weight bold))))
-           ;; transient
-           `(transient-separator ((,class (:foreground ,s-line))))
-           ;; symbol-overlay
-           `(symbol-overlay-default-face ((,class :inherit unspecified :foreground ,magenta)))
-           ;; woman
-           `(woman-bold ((,class (:inherit Man-overstrike))))
-           `(woman-italic ((,class (:inherit Man-underline)))))
-          ;; (custom-theme-set-variables theme-name)
-          ))
+  (progn
+    (setq solarized-custom-faces
+          '("My personal solarized theme customizations"
+            (custom-theme-set-faces
+             theme-name
+             `(cursor ((,class (:background ,blue))))
+             `(fringe ((,class (:foreground ,s-line))))
+             `(header-line ((,class (:foreground ,yellow :underline ,base02 :weight bold))))
+             `(minibuffer-prompt ((,class (:foreground ,yellow))))
+             `(mode-line ((,class (:background ,blue-2bg))))
+             `(mode-line-buffer-id ((,class (:foreground ,yellow :weight bold))))
+             `(mode-line-inactive ((,class (:background ,base02))))
+             ;; dired
+             `(dired-header ((,class (:foreground ,yellow :underline t :weight bold))))
+             ;; transient
+             `(transient-separator ((,class (,@(and (>= emacs-major-version 27) '(:extend t))
+                                             :background ,s-line))))
+             ;; symbol-overlay - TODO add other faces
+             `(symbol-overlay-default-face ((,class :inherit unspecified :foreground ,magenta)))
+             ;; woman
+             `(woman-bold ((,class (:inherit Man-overstrike))))
+             `(woman-italic ((,class (:inherit Man-underline)))))))
 
-  (load-theme 'solarized-dark t)
+    (load-theme 'solarized-dark t)
 
-  (solarized-with-color-variables
-    'dark 'solarized-dark solarized-dark-color-palette-alist solarized-custom-faces))
+    (solarized-with-color-variables
+      'dark 'solarized-dark solarized-dark-color-palette-alist solarized-custom-faces))
+  )
