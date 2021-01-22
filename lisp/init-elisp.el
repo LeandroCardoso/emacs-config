e7850229493410f8d392887a42059cd9aa566af5
Leandro Cardoso <leandrocardoso@gmail.com>
2021-01-21 21:37:30 -0300

    elisp: add alternative key to eval-defun

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -1,3 +1,5 @@
 (require 'elisp-mode)
 (require 'mode-local)
 (setq-mode-local emacs-lisp-mode sentence-end-double-space t)
+
+(define-key emacs-lisp-mode-map (kbd "C-c C-c") 'eval-defun) ; eval-defun is also in C-M-x
