6fedfa06b200e3506bec758718d5beb72186d14f
Leandro Cardoso <leandrocardoso@gmail.com>
2021-04-15 12:39:49 -0300

    eshell: added alias

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -1,1 +1,6 @@
+(with-eval-after-load 'eshell
+  ;; Changing from the default value (~/.emacs.d/eshell/alias) because the eshell directory is in
+  ;; the .gitignore, so I can ignore the transient files inside this directory.
+  (setq eshell-aliases-file (concat user-emacs-directory "eshell_alias")))
+
 (global-set-key (kbd "C-x C-$") 'eshell)
