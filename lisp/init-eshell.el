e859166cc7c268c4c9af7babab08e3845200857f
Leandro Cardoso <leandrocardoso@gmail.com>
2021-04-15 13:45:37 -0300

    eshell: add watch to visual commands

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -1,6 +1,7 @@
 (with-eval-after-load 'eshell
   ;; Changing from the default value (~/.emacs.d/eshell/alias) because the eshell directory is in
   ;; the .gitignore, so I can ignore the transient files inside this directory.
-  (setq eshell-aliases-file (concat user-emacs-directory "eshell_alias")))
+  (setq eshell-aliases-file (concat user-emacs-directory "eshell_alias"))
+  (add-to-list 'eshell-visual-commands "watch"))
 
 (global-set-key (kbd "C-x C-$") 'eshell)
