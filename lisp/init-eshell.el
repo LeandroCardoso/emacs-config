7f2adeb098fcb2f1d733ab517e9f21d5199137ce
Leandro Cardoso <leandrocardoso@gmail.com>
2021-04-15 21:44:43 -0300

    eshell: fix initialization

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -1,7 +1,9 @@
-(with-eval-after-load 'eshell
-  ;; Changing from the default value (~/.emacs.d/eshell/alias) because the eshell directory is in
-  ;; the .gitignore, so I can ignore the transient files inside this directory.
-  (setq eshell-aliases-file (concat user-emacs-directory "eshell_alias"))
-  (add-to-list 'eshell-visual-commands "watch"))
+(require 'em-alias)
+;; Changing from the default value (~/.emacs.d/eshell/alias) because the eshell directory is in the
+;; .gitignore, so I can ignore the transient files inside this directory.
+(setq eshell-aliases-file (concat user-emacs-directory "eshell_alias"))
+
+(require 'em-term)
+(add-to-list 'eshell-visual-commands "watch")
 
 (global-set-key (kbd "C-x C-$") 'eshell)
