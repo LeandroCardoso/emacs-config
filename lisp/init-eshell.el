a9b963bce4baa5890f9f50e2359611003d589e10
Leandro Cardoso <leandrocardoso@gmail.com>
2021-04-15 22:03:21 -0300

    shell, eshell: add *-other-window functions

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -6,4 +6,14 @@
 (require 'em-term)
 (add-to-list 'eshell-visual-commands "watch")
 
+(defun eshell-other-window (&optional arg)
+  "Like `eshell', but creates a new window or reuses an existing
+one."
+  (interactive "P")
+  (if (one-window-p)
+      (split-window)
+    (other-window 1))
+  (eshell arg))
+
 (global-set-key (kbd "C-x C-$") 'eshell)
+(global-set-key (kbd "C-x 4 C-$") 'eshell-other-window)
--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -1,4 +1,14 @@
+(defun shell-other-window (&optional buffer)
+  "Like `shell', but creates a new window or reuses an existing
+one."
+  (interactive "P")
+  (if (one-window-p)
+      (split-window)
+    (other-window 1))
+  (shell buffer))
+
 (global-set-key (kbd "C-x $") 'shell) ; original is set-selective-display
+(global-set-key (kbd "C-x 4 $") 'shell-other-window)
 
 (when (require 'bash-completion nil t)
   (add-hook 'shell-dynamic-complete-functions 'bash-completion-dynamic-complete))
