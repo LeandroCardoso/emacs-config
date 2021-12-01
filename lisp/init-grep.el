1b0021a69d5e466b281c1fed3b559784c397f874
Leandro Cardoso <leandro.cardoso@capgemini.com>
2021-12-01 15:47:55 -0300

    grep: reworked aliases and added cx (c-extra)

--- lisp/init-grep.el
+++ lisp/init-grep.el
@@ -1,12 +1,15 @@
 (with-eval-after-load "grep"
   (setq grep-save-buffers nil)
 
-  ;; Remove c++ aliases and add new ones with *.c files in the begging of the list to get higher
-  ;; priority.
-  (assq-delete-all (car (assoc "cc" grep-files-aliases)) grep-files-aliases)
-  (assq-delete-all (car (assoc "cchh" grep-files-aliases)) grep-files-aliases)
-  (push '("cc" . "*.cc *.cxx *.cpp *.[Cc] *.CC *.c++") grep-files-aliases)
-  (push '("cchh" . "*.cc *.[ch]xx *.[ch]pp *.[CHch] *.CC *.HH *.[ch]++") grep-files-aliases)
+  (let ((cc "*.cc *.cxx *.cpp *.[Cc] *.CC *.c++")
+        (hh "*.hxx *.hpp *.[Hh] *.HH *.h++")
+        (cext "*.def *.rc"))
+    (setq grep-files-aliases (assoc-delete-all "cc" grep-files-aliases))
+    (setq grep-files-aliases (assoc-delete-all "cchh" grep-files-aliases))
+    (setq grep-files-aliases (assoc-delete-all "cx" grep-files-aliases))
+    (push `("cc" . ,cc) grep-files-aliases)
+    (push `("cchh" . ,(concat cc hh)) grep-files-aliases)
+    (push `("cx" . ,(concat cc hh cext)) grep-files-aliases))
 
   (add-to-list 'grep-files-aliases '("cs" . "*.cs"))
 
