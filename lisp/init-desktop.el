051b3a7728d94bef2ce4215afe9c6cdbbea57b6f
Leandro Cardoso <leandro.cardoso@capgemini.com>
2022-05-05 14:31:09 -0300

    fix clean-buffer-list running inadvertently during desktop-save

--- lisp/init-desktop.el
+++ lisp/init-desktop.el
@@ -1,4 +1,3 @@
 (with-eval-after-load "desktop"
-  (add-hook 'desktop-save-hook 'clean-buffer-list)
   (setq desktop-save 'ask-if-exists)
   (desktop-save-mode))
--- lisp/init-midnight.el
+++ lisp/init-midnight.el
@@ -1,24 +1,17 @@
 (require 'midnight)
 
-(setq clean-buffer-list-delay-general 1)
-(setq clean-buffer-list-last-execution (current-time))
+(setq clean-buffer-list-delay-general 2)
+
+(defun clean-buffer-list-check-idle-time ()
+  "Return t if emacs idle time is less than the
+`clean-buffer-list-delay-general'."
+  (< (round (float-time (or (current-idle-time) '(0 0 0))))
+     (* clean-buffer-list-delay-general 24 60 60)))
 
 ;; Don't cleanup the buffer list when I am idle for more than 1 day, like weekends and holidays.
-(defun clean-buffer-list-check-idle-time ()
-  "Like `clean-buffer-list', but it only excecutes when computer
-is not idle."
-  (when (and (< (round (float-time (or (current-idle-time) '(0 0 0))))
-                (* clean-buffer-list-delay-general 24 60 60))
-             ;; Avoid execution when computer was resumed from suspension, which usually leads to
-             ;; all buffers to be killed.
-             (< (- (round (float-time (current-time)))
-                   (round (float-time clean-buffer-list-last-execution)))
-                (+ (* clean-buffer-list-delay-general 24 60 60)
-                   (* 2 60 60))))
-    (clean-buffer-list))
-  (setq clean-buffer-list-last-execution (current-time)))
+(advice-add 'clean-buffer-list :before-while #'clean-buffer-list-check-idle-time)
 
-(remove-hook 'midnight-hook 'clean-buffer-list)
-(add-hook 'midnight-hook 'clean-buffer-list-check-idle-time)
+;; Add with DEPTH=-10 because `clean-buffer-list' should run before the `desktop-kill'
+(add-hook 'kill-emacs-hook 'clean-buffer-list -10)
 
 (midnight-mode t)
