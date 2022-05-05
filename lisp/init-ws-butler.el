49d917859297c042272c49607acdd0c89acee8cf
Leandro Cardoso <leandro.cardoso@capgemini.com>
2022-05-05 14:32:44 -0300

    ws-butler: disabled mode during ediff
    to avoid whitespace changes during merge

--- lisp/init-ws-butler.el
+++ lisp/init-ws-butler.el
@@ -1,20 +1,18 @@
 (when (require 'ws-butler nil t)
   (setq ws-butler-keep-whitespace-before-point nil)
 
-  (defun ws-butler-enable-ediff-buffer-C ()
-    (when (and ediff-buffer-C
-               ws-butler-global-mode
+  (defun ws-butler-mode-enable ()
+    (when (and ws-butler-global-mode
                (not (apply (function derived-mode-p) ws-butler-global-exempt-modes)))
-      (with-current-buffer ediff-buffer-C
-        (ws-butler-mode))))
+      (ws-butler-mode)))
 
-  (defun ws-butler-disable-ediff-buffer-C ()
-    (when ediff-buffer-C
-      (with-current-buffer ediff-buffer-C
-        (ws-butler-mode -1))))
+  (defun ws-butler-mode-disable ()
+    (ws-butler-mode -1))
 
-  (add-hook 'ediff-startup-hook 'ws-butler-disable-ediff-buffer-C)
-  (add-hook 'ediff-cleanup-hook 'ws-butler-enable-ediff-buffer-C)
-  (add-hook 'ediff-suspend-hook 'ws-butler-enable-ediff-buffer-C)
+  (defun ws-butler-global-mode-disable ()
+    (ws-butler-global-mode -1))
+
+  (add-hook 'ediff-startup-hook 'ws-butler-mode-disable)
+  (add-hook 'ediff-cleanup-hook 'ws-butler-mode-enable)
 
   (ws-butler-global-mode))
