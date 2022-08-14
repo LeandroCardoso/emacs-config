b2d67ba3c7ceff51e4f88b3ef41a7ad29cb20f93
Leandro Cardoso <leandrocardoso@gmail.com>
2022-08-14 03:56:07 -0300

    find-file: renamed sudo-find-file

--- lisp/init-find-files.el
+++ lisp/init-find-files.el
@@ -1,16 +1,15 @@
 (setq ff-case-fold-search t)
 
-(unless (eq system-type 'windows-nt)
-  (defun find-file-sudo (&optional arg)
-    "Like `find-file', but as root.
+(defun sudo-find-file (&optional arg)
+  "Like `find-file', but as root.
 
 With a prefix ARG re-open the current file as root."
-    (interactive "P")
-    (if (and arg buffer-file-name)
-        (let ((p (point)))
-          (find-file (concat "/sudo:root@localhost:" buffer-file-name))
-          (goto-char p))
-      (find-file (concat "/sudo:root@localhost:"
-                         (read-file-name "Find file (as root): "))))))
+  (interactive "P")
+  (if (and arg buffer-file-name)
+      (let ((p (point)))
+        (find-file (concat "/sudo:root@localhost:" buffer-file-name))
+        (goto-char p))
+    (find-file (concat "/sudo:root@localhost:"
+                       (read-file-name "Find file (as root): ")))))
 
 (global-set-key (kbd "C-M-o") 'ff-find-other-file) ;; default is split-line
