495d0796cc62e38c67776d6b1a4e6fc4846f0020
Leandro Cardoso <leandrocardoso@gmail.com>
2022-08-16 01:37:06 -0300

    project: add user's emacs lisp packages to custom directory project

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -1,4 +1,17 @@
 (with-eval-after-load 'project
+  ;; Workaround for emacs 27.1 - Emacs 27.1 does not treat a Git submodule as a project root.
+  ;; Unfortunately I need this functionality, so I am restoring the 26.3 implementation until 28.1
+  ;; when an user option is been introduced.
+  (defun project-try-vc-simple (dir)
+    (let* ((backend (ignore-errors (vc-responsible-backend dir)))
+           (root (and backend (ignore-errors
+                                (vc-call-backend backend 'root dir)))))
+      (and root (cons 'vc root))))
+
+  (add-to-list 'project-find-functions 'project-try-vc-simple)
+
+  ;; Custom directory project
+
   (defcustom project-simple-root-list nil
     "List of directories root projects.")
 
@@ -35,17 +48,8 @@
   ;; directory directly, so I am using a random library from emacs lisp directory.
   (add-to-list 'project-simple-root-list (file-name-directory (find-library-name "misc")))
 
-
-  ;; Workaround for emacs 27.1 - Emacs 27.1 does not treat a Git submodule as a project root.
-  ;; Unfortunately I need this functionality, so I am restoring the 26.3 implementation until 28.1
-  ;; when an user option is been introduced.
-  (defun project-try-vc-simple (dir)
-    (let* ((backend (ignore-errors (vc-responsible-backend dir)))
-           (root (and backend (ignore-errors
-                                (vc-call-backend backend 'root dir)))))
-      (and root (cons 'vc root))))
-
-  (add-to-list 'project-find-functions 'project-try-vc-simple)
+  ;; Add user's Emacs Lisp packages
+  (add-to-list 'project-root-list (expand-file-name package-user-dir))
 
   (global-set-key (kbd "C-x C-M-f") 'project-find-file)
   (global-set-key (kbd "C-x C-M-g") 'project-find-regexp))
