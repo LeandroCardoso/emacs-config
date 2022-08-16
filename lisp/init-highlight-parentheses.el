55a941ac55dff16dfb08c7f9e17ed97474cd88c4
Leandro Cardoso <leandro.cardoso@capgemini.com>
2022-08-16 15:08:47 -0300

    highlight-parentheses: enabled in minibuffer

--- lisp/init-highlight-parentheses.el
+++ lisp/init-highlight-parentheses.el
@@ -1,3 +1,4 @@
 (when (require 'highlight-parentheses nil t)
   (setq hl-paren-delay 0.25)
-  (global-highlight-parentheses-mode))
+  (global-highlight-parentheses-mode)
+  (add-hook 'minibuffer-setup-hook #'highlight-parentheses-minibuffer-setup))
