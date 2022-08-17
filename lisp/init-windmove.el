31d8ee0c85b291f69470653903801bc715e7b096
Leandro Cardoso <leandro.cardoso@capgemini.com>
2022-08-17 18:11:07 -0300

    windmove: disable unused features

--- lisp/init-windmove.el
+++ lisp/init-windmove.el
@@ -1,11 +1,7 @@
 ;; enable windmove - CTRL is only modifier not used by org-mode
 (windmove-default-keybindings 'ctrl)
 (when (>= emacs-major-version 27)
-  (windmove-delete-default-keybindings 'C-x 'ctrl)
-  (windmove-display-default-keybindings '(ctrl meta))
   (windmove-swap-states-default-keybindings '(ctrl shift)))
-
-(setq windmove-create-window t)
 
 ;; framemove is a windmove extension for frames.
 (when (require 'framemove nil t)
