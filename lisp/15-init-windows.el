8379755d93f45a075522924356517767ac69f35f
Leandro Cardoso <leandro.cardoso@capgemini.com>
2022-08-17 12:26:20 -0300

    windows: add Git For Windows to exec-path

--- lisp/15-init-windows.el
+++ lisp/15-init-windows.el
@@ -40,6 +40,9 @@
   (w32-add-to-path (expand-file-name "windows_bin/" user-emacs-directory))
   (add-to-list 'exec-path (expand-file-name "windows_bin/" user-emacs-directory))
 
+  ;; Add Git For Windows to exec-path
+  (add-to-list 'exec-path "C:/Program Files/Git/cmd")
+
   ;; Workaround for ssh
   (setenv "SSH_ASKPASS" "c:/msys64/usr/bin/sshpass.exe")
 
