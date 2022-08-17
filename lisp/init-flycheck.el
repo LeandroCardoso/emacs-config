1e5a8f5669a34e8bf35aca48c45c306359b1b326
Leandro Cardoso <leandro.cardoso@capgemini.com>
2022-08-17 17:38:52 -0300

    flycheck: enable idle-buffer-switch on windows

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,10 +1,11 @@
 (when (require 'flycheck nil t)
   (setq flycheck-check-syntax-automatically (if (eq system-type 'windows-nt)
-                                                '(save idle-change)
+                                                '(save idle-change idle-buffer-switch)
                                               '(save idle-change mode-enabled)))
   (setq flycheck-checker-error-threshold nil)
   (setq flycheck-completing-read-function 'ido-completing-read)
   (setq flycheck-global-modes '(c-mode c++-mode csharp-mode js-mode typescript-mode))
+  (setq flycheck-idle-buffer-switch-delay (if (eq system-type 'windows-nt) 30 5))
   (setq flycheck-idle-change-delay (if (eq system-type 'windows-nt) 30 5))
   (setq flycheck-mode-line-prefix "!")
 
@@ -15,5 +16,4 @@
   ;; unusedStructMember is annoying in header files
   (setq flycheck-cppcheck-suppressions '("unusedStructMember"))
 
-  (global-flycheck-mode)
-  )
+  (global-flycheck-mode))
