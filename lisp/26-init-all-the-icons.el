9cfec9eebfb6237d78d6c9a8251742b07074c733
Leandro Cardoso <leandrocardoso@gmail.com>
2022-08-17 01:10:04 -0300

    replace moody by doom-modeline and all-the-icons

---
+++ lisp/26-init-all-the-icons.el
@@ -0,0 +1,3 @@
+(when (and (require 'all-the-icons nil t) (display-graphic-p))
+  (setq inhibit-compacting-font-caches t)
+  (setq all-the-icons-fonts-subdirectory "all-the-icons"))
---
+++ lisp/27-init-doom-modeline.el
@@ -0,0 +1,4 @@
+(when (require 'doom-modeline nil t)
+  (setq doom-modeline-height 30)
+  (setq doom-modeline-indent-info t)
+  (doom-modeline-mode 1))
