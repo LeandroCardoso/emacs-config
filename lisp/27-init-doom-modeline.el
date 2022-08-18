3c0f9f8681f3f86dbb45730d401ff98bc2de04cd
Leandro Cardoso <leandro.cardoso@capgemini.com>
2022-08-18 15:04:47 -0300

    doom-modeline: set buffer-file-name-style to buffer-name and vcs-max-length to 22

--- lisp/27-init-doom-modeline.el
+++ lisp/27-init-doom-modeline.el
@@ -1,4 +1,6 @@
 (when (require 'doom-modeline nil t)
+  (setq doom-modeline-buffer-file-name-style 'buffer-name)
   (setq doom-modeline-height 30)
   (setq doom-modeline-indent-info t)
+  (setq doom-modeline-vcs-max-length 22)
   (doom-modeline-mode 1))
