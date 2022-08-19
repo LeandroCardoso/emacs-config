63042a93f0a2bc8470788d125e45ea2251bc30b6
Leandro Cardoso <leandro.cardoso@capgemini.com>
2022-08-19 12:03:38 -0300

    Delete old elisp compiled files

--- init.el
+++ init.el
@@ -9,14 +9,26 @@
 (when (< emacs-major-version 27)
   (package-initialize))
 
-;; Load all emacs list (*.el/*.elc) files sorted by name at ~/.emacs.d/lisp. Sub-directories and
-;; files starting with underline or dot are ignored. If a compiled elisp file exist and it is not
-;; outdated, then load it instead of the non-compiled one.
 (setq load-prefer-newer t)
 (let ((local-lisp (expand-file-name "lisp" user-emacs-directory)))
   (add-to-list 'load-path local-lisp)
-  (mapc 'load (mapcar 'file-name-base (directory-files local-lisp nil "^[^_\\.].*\\.el$")))
-  (byte-recompile-directory local-lisp 0))
+
+  ;; Compile all elisp source files (.el)
+  (message "Compiling elisp source files from %s" local-lisp)
+  (byte-recompile-directory local-lisp 0)
+
+  ;; Delete old elisp compiled files (.elc) that doesn't have a eslisp source file (.el) associated
+  (message "Deleting unused elisp compiled files from %s" local-lisp)
+  (dolist (file (directory-files local-lisp nil ".*\\.elc$"))
+    (unless (file-exists-p (expand-file-name (file-name-with-extension file ".el") local-lisp))
+      (message "Deleting %s" file)
+      (delete-file (expand-file-name file local-lisp))))
+
+  ;; Load all elisp files sorted by name. Sub-directories and files starting with underline or dot
+  ;; are ignored
+  (message "Loading elisp source files from %s" local-lisp)
+  (dolist (file (directory-files local-lisp nil "^[^_\\.].*\\.el$"))
+    (load (file-name-sans-extension file))))
 
 (setq gc-cons-threshold (car (get 'gc-cons-threshold 'standard-value)))
 (message "emacs started in %s" (emacs-init-time))
