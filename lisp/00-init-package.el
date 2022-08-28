9dc2dbc3fb7501a2d0fcc76fb460a6e1e84a9cc1
Leandro Cardoso <leandrocardoso@gmail.com>
2022-08-28 03:13:24 -0300

    package: added nongnu

--- lisp/00-init-package.el
+++ lisp/00-init-package.el
@@ -3,8 +3,9 @@
 (add-to-list 'package-archives '("melpa stable" . "https://stable.melpa.org/packages/"))
 
 (setq package-archive-priorities
-      '(("melpa"        . 2)
-        ("melpa stable" . 1)
-        ("gnu"          . 0)))
+      '(("melpa"        . 3)
+        ("nongnu"       . 2)
+        ("gnu"          . 1)
+        ("melpa stable" . 0)))
 
 (global-set-key (kbd "C-x C-p") 'list-packages) ; default is mark-page
