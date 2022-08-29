79d6b7f367aab5a794962c90a1748a6859946762
Leandro Cardoso <leandrocardoso@gmail.com>
2022-08-28 21:57:50 -0300

    occur: renamed file and remove key

--- lisp/init-occur.el
+++ lisp/init-occur.el
@@ -4,8 +4,5 @@
 (define-key occur-mode-map (kbd "<tab>") 'occur-next)
 (define-key occur-mode-map (kbd "<backtab>") 'occur-prev)
 
-(define-key occur-mode-map (kbd "n") 'occur-next)
-(define-key occur-mode-map (kbd "p") 'occur-prev)
-
 (define-key occur-mode-map (kbd "k") 'keep-lines)
 (define-key occur-mode-map (kbd "f") 'flush-lines)
