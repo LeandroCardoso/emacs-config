a46e2b17478b0ee85f42a7828ff64ec16c930311
Leandro Cardoso <leandrocardoso@gmail.com>
2022-08-31 22:58:11 -0300

    project: added project-find-regexp-with-arg

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -2,6 +2,17 @@
   ;; settings
   (setq project-vc-merge-submodules nil)
 
+  ;; functions
+  (defun project-find-regexp-with-arg ()
+    "Call `project-find-regexp' with \\[universal-argument] prefix."
+    (interactive)
+    (let ((current-prefix-arg '(4)))
+      (call-interactively 'project-find-regexp)))
+
+  ;; keys
+  (global-set-key (kbd "C-x p C-g") 'project-find-regexp-with-arg)
+
+
   ;; Custom directory project
 
   (defcustom project-simple-root-list nil
