98dccfc05312442a2d6a0f87c8a34bdff0eb06ce
Leandro Cardoso <leandrocardoso@gmail.com>
2022-08-31 22:59:21 -0300

    xref: update to emacs 28.1
    use ripgrep
    save the xref id/pattern in kill-ring
    add keys to rename-uniquely and keep/flush-lines

--- lisp/init-xref.el
+++ lisp/init-xref.el
@@ -1,4 +1,20 @@
 (with-eval-after-load "xref"
-  (define-key xref--xref-buffer-mode-map (kbd "<tab>") 'xref-next-line) ; original is xref-quit-and-goto-xref
+  ;; settings
+  (setq xref-search-program 'ripgrep)
+  (setq xref-show-definitions-function 'xref-show-definitions-completing-read)
+
+  ;; save the xref id/pattern in kill-ring
+  (defun xref-save-id-advice (id &optional arg1 arg2 arg3)
+    (kill-new id))
+
+  (advice-add 'xref--find-definitions :after #'xref-save-id-advice)
+  (advice-add 'xref--find-xrefs :after #'xref-save-id-advice)
+
+  ;; keys
+  (define-key xref--xref-buffer-mode-map (kbd "<tab>") 'xref-next-line)
   (define-key xref--xref-buffer-mode-map (kbd "<backtab>") 'xref-prev-line)
-  (define-key xref--xref-buffer-mode-map (kbd "M-<return>") 'xref-quit-and-goto-xref))
+  (define-key xref--xref-buffer-mode-map (kbd "M-<return>") 'xref-quit-and-goto-xref)
+
+  (define-key xref--xref-buffer-mode-map (kbd "u") 'rename-uniquely)
+  (define-key xref--xref-buffer-mode-map (kbd "k") 'keep-lines)
+  (define-key xref--xref-buffer-mode-map (kbd "f") 'flush-lines))
