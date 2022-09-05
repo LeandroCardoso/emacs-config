4e25922d9e00ac166a53799191e332bcd489d59b
Leandro Cardoso <leandrocardoso@gmail.com>
2022-09-05 18:41:21 -0300

    enabled global-goto-address-mode

--- lisp/init-goto-addr.el
+++ lisp/init-goto-addr.el
@@ -1,4 +1,1 @@
-(add-hook 'prog-mode-hook 'goto-address-prog-mode)
-
-;; goto-addr does not have a key-binding map variable, so I am using the prog-mode-map.
-(define-key prog-mode-map (kbd "C-c <return>") 'goto-address-at-point)
+(global-goto-address-mode)
