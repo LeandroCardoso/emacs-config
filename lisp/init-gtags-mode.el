127b3a274ca9c468250bee67bf7a41ecb9d0c01c
Leandro Cardoso <leandro.cardoso@capgemini.com>
2022-12-14 16:37:09 -0300

    gtags: added key to gtags-mode-update

--- lisp/init-gtags-mode.el
+++ lisp/init-gtags-mode.el
@@ -4,6 +4,8 @@
   ;; Force .h files to be treated as a C++
   (setenv "GTAGSFORCECPP" "1")
 
+  (global-set-key (kbd "C-x p t") 'gtags-mode-update)
+
   ;; Use bash shell when calling global, because it fixes the annoying "^M" that can be displayed at
   ;; end of lines.
   (when (eq system-type 'windows-nt)
