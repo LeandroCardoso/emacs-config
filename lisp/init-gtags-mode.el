f110981805510a0bf1d2ca4386b2d095f5a0e7e6
Leandro Cardoso <leandro.cardoso@capgemini.com>
2022-12-21 15:42:36 -0300

    gtags: added gtags-mode-project-create

--- lisp/init-gtags-mode.el
+++ lisp/init-gtags-mode.el
@@ -1,10 +1,24 @@
 (when (require 'gtags-mode nil t)
+  (require 'project)
+
+  (defun gtags-mode-project-create ()
+    "Create a GLOBAL GTAGS file in the root directory of the current
+ project asynchronously.
+
+When no project is found the `default-directory', ask the user
+for a directory.
+
+See `gtags-mode-create'."
+    (interactive)
+    (gtags-mode-create (project-root (project-current t))))
+
   (gtags-mode)
 
   ;; Force .h files to be treated as a C++
   (setenv "GTAGSFORCECPP" "1")
 
-  (global-set-key (kbd "C-x p t") 'gtags-mode-update)
+  (global-set-key (kbd "C-x p T") 'gtags-mode-update)
+  (global-set-key (kbd "C-x p t") 'gtags-mode-project-create)
 
   ;; Use bash shell when calling global, because it fixes the annoying "^M" that can be displayed at
   ;; end of lines.
