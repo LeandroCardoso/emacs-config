e825409827c634e50044795a6d883f35b3646248
Leandro Cardoso <leandrocardoso@gmail.com>
2023-01-03 23:06:47 -0300

    project: renamed project-query-regexp

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -3,13 +3,14 @@
   (setq project-vc-merge-submodules nil)
 
   ;; functions
-  (defun project-find-regexp-in-directory ()
-    "Like `project-find-regexp', but you is always asked for a directory to search in."
+  (defun project-query-regexp ()
+    "Find all matches for REGEXP in the current project.
+
+See `project-find-regexp'."
     (interactive)
     (let ((current-prefix-arg '(4)))
       (call-interactively 'project-find-regexp)))
 
   ;; keys
-  (global-set-key (kbd "C-M-g") 'project-find-regexp)
-  (global-set-key (kbd "C-x p M-g") 'project-find-regexp-in-directory)
-  (global-set-key (kbd "C-M-G") 'project-find-regexp-in-directory))
+  (global-set-key (kbd "C-x p q") 'project-query-regexp)
+  (global-set-key (kbd "C-M-g") 'project-query-regexp))
