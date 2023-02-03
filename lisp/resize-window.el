9df8ba33729ee121ab0f9c8241dd641edf1318dc
Leandro Cardoso <leandro.cardoso@capgemini.com>
2023-02-03 15:53:08 -0300

    resize-window: use window width/height, instead of frame width/height

--- lisp/resize-window.el
+++ lisp/resize-window.el
@@ -4,7 +4,7 @@
 `shrink-window-horizontally+'. Value specifies either an
 integer (the number of lines to enlarge or shrink the selected
 window), a floating point number between 0.1 and 0.9 (the
-fraction of the current frame height)."
+fraction of the current window height)."
   :type 'float
   :group 'resize-window)
 
@@ -14,7 +14,7 @@ fraction of the current frame height)."
 `shrink-window-horizontally+'. Value specifies either an
 integer (the number of columns to enlarge or shrink the selected
 window), a floating point number between 0.1 and 0.9 (the
-fraction of the current frame width)."
+fraction of the current window width)."
   :type 'float
   :group 'resize-window)
 
@@ -22,16 +22,16 @@ fraction of the current frame width)."
 (defun enlarge-window+ (delta &optional horizontal)
   "If DELTA is an integer, make the selected window DELTA lines taller.
 If DELTA is a float between 0.1 and 0.9 make the selected window
-the fraction of the current frame height tallter. If the function
+the fraction of the current window height taller. If the function
 was called interactively and no argument is given, make the
 selected window `resize-window-vertical-step' taller.
 
 If optional argument HORIZONTAL is non-nil and DELTA is an
-integer make selected window wider by DELTA columns. If
-optional argument HORIZONTAL is non-nil and DELTA is a float
-between 0.1 and 0.9 make the selected window the fraction of the
-current frame width wider. If the function was called
-interactively and no argument is given, make selected window
+integer make selected window wider by DELTA columns. If optional
+argument HORIZONTAL is non-nil and DELTA is a float between 0.1
+and 0.9 make the selected window the fraction of the current
+window width wider. If the function was called interactively and
+no argument is given, make selected window
 `resize-window-horizontal-step' wider.
 
 If DELTA is negative, shrink selected window by -DELTA."
@@ -43,7 +43,7 @@ If DELTA is negative, shrink selected window by -DELTA."
                                  ((null delta) resize-window-step)
                                  (t delta))))
     (enlarge-window (if (floatp delta-or-default)
-                        (let ((dt-pxl (truncate (* (if horizontal (frame-width) (frame-height))
+                        (let ((dt-pxl (truncate (* (if horizontal (window-width) (window-height))
                                                   delta-or-default))))
                           (if (= dt-pxl 0) 1 dt-pxl))
                       delta-or-default)
@@ -53,15 +53,15 @@ If DELTA is negative, shrink selected window by -DELTA."
 (defun shrink-window+ (delta &optional horizontal)
   "If DELTA is an integer, make the selected window DELTA lines shorter.
 If DELTA is a float between 0.1 and 0.9 make the selected window
-the fraction of the current frame height shorter. If the function
-was called interactively and no argument is given, make the
-selected window `resize-window-vertical-step' shorter.
+the fraction of the current window height shorter. If the
+function was called interactively and no argument is given, make
+the selected window `resize-window-vertical-step' shorter.
 
 If optional argument HORIZONTAL is non-nil and DELTA is an
 integer make selected window narrower by DELTA columns. If
 optional argument HORIZONTAL is non-nil and DELTA is a float
 between 0.1 and 0.9 make the selected window the fraction of the
-current frame width wider. if the function was called
+current window width wider. if the function was called
 interactively and no argument is given, make selected window
 `resize-window-horizontal-step' narrower.
 
@@ -74,11 +74,12 @@ If DELTA is negative, enlarge selected window by -DELTA."
 
 
 (defun enlarge-window-horizontally+ (delta)
-  "If DELTA is an integer make selected window wider by
-DELTA columns. If DELTA is a float between 0.1 and 0.9 make the
-selected window the fraction of the current frame width wider. If
-the function was called interactively and no argument is given,
-make selected window `resize-window-horizontal-step' wider.
+  "If DELTA is an integer make selected window wider by DELTA
+columns. If DELTA is a float between 0.1 and 0.9 make the
+selected window the fraction of the current window width
+wider. If the function was called interactively and no argument
+is given, make selected window `resize-window-horizontal-step'
+wider.
 
 If DELTA is negative, shrink selected window by -DELTA."
   (interactive "P")
@@ -86,12 +87,12 @@ If DELTA is negative, shrink selected window by -DELTA."
 
 
 (defun shrink-window-horizontally+ (delta)
-  "If DELTA is an integer make selected window narrower by
-DELTA columns. If DELTA is a float between 0.1 and 0.9 make the
-selected window the fraction of the current frame width narrower.
-If the function was called interactively and no argument is
-given, make selected window `resize-window-horizontal-step'
-narrower.
+  "If DELTA is an integer make selected window narrower by DELTA
+columns. If DELTA is a float between 0.1 and 0.9 make the
+selected window the fraction of the current window width
+narrower.  If the function was called interactively and no
+argument is given, make selected window
+`resize-window-horizontal-step' narrower.
 
 If DELTA is negative, shrink selected window by -DELTA."
   (interactive "P")
