74e8391d4dfd939e813ab3fcb21c3c041ae3e1fd
Leandro Cardoso <leandrocardoso@gmail.com>
2023-02-08 17:03:52 -0300

    ui: increase transparency

--- lisp/20-init-ui.el
+++ lisp/20-init-ui.el
@@ -198,7 +198,7 @@ in all frames."
 (setq frame-inhibit-implied-resize t) ;; never resize the frame
 (setq initial-frame-alist '((fullscreen . maximized) (vertical-scroll-bars . nil)))
 (setq default-frame-alist initial-frame-alist)
-(setq window-system-default-frame-alist '((x . ((alpha . 98)))))
+(setq window-system-default-frame-alist '((x . ((alpha . 97)))))
 
 ;; key-bindings
 
