3f2dd701428ddadbc0b1153f094943ed1bb97b5f
Leandro Cardoso <leandrocardoso@gmail.com>
2023-02-16 22:21:56 -0300

    files: improved copy-file-or-buffer-name-as-kill and copy-file-or-buffer-name-directory-as-kill

--- lisp/init-files.el
+++ lisp/init-files.el
@@ -3,27 +3,40 @@
   (when (file-directory-p path)
     (process-lines find-program (expand-file-name path) "-type" "d")))
 
-(defun copy-buffer-name-as-kill (&optional arg)
-  "If the current buffer is a file visited buffer, show the full
- path of the file and without parameter ARG copy it to the kill
- ring, with paramater ARG copy just the file name to the kill
- ring.
+(defun copy-file-or-buffer-name-as-kill (&optional arg)
+  "If the current buffer is a file visited buffer, copy the file
+name of the current buffer to the kill ring. With parameter ARG,
+copy the full file name path of the current buffer.
 
-If the current buffer is not a file visited buffer, show the
-current default directory and without parameter ARG copy it to
-the kill ring, wit parameter ARG copy the buffer name to the kill
-ring."
+If the current buffer is not a file visited buffer, copy the
+current buffer name to the kill ring."
   (interactive "P")
-  (kill-new (if buffer-file-name
-                (if arg
-                    buffer-file-name
-                  (file-name-nondirectory buffer-file-name))
-              (if arg
-                  (buffer-name)
-                default-directory)))
-  (if buffer-file-name
-      (message "File name %s" buffer-file-name)
-    (message "Directory %s" default-directory)))
+  (let ((name (if buffer-file-name
+                  (if arg
+                      buffer-file-name
+                    (file-name-nondirectory buffer-file-name))
+                (buffer-name))))
+  (kill-new name)
+  (message "%s name %s" (if buffer-file-name "File" "Buffer") name)))
+
+(defun copy-file-or-buffer-name-directory-as-kill (&optional arg)
+  "If the current buffer is a file visited buffer, copy the
+directory component of the current file to the kill ring.
+
+If the current buffer is not a file visited buffer, copy the
+current default directory of the current buffer to the kill ring.
+
+With parameter ARG, convert the directory to absolute, and
+canonicalize it."
+  (interactive "P")
+  (let* ((dir (if buffer-file-name
+                  (file-name-directory buffer-file-name)
+                default-directory))
+         (dir-exp (if arg
+                      (expand-file-name dir)
+                    dir)))
+    (kill-new dir-exp)
+    (message "Directory %s" dir-exp)))
 
 (defun rename-buffer-and-file ()
   "Rename the current buffer and the file name when it is
@@ -71,7 +84,8 @@ See `backup-buffer'."
 (setq confirm-kill-processes nil)
 
 ;; key bindings
-(global-set-key (kbd "C-x x w") 'copy-buffer-name-as-kill)
+(global-set-key (kbd "C-x x w") 'copy-file-or-buffer-name-as-kill)
+(global-set-key (kbd "C-x x W") 'copy-file-or-buffer-name-directory-as-kill)
 (global-set-key (kbd "C-x x k") 'make-backup-buffer)
 (global-set-key (kbd "C-x x r") 'rename-buffer-and-file) ; replace rename-buffer
 (global-set-key (kbd "C-x x G") 'revert-buffer-with-fine-grain)
