0f5e916369f1296a9dd6d5383bc8c98664dba2a4
Leandro Cardoso <leandro.cardoso@capgemini.com>
2023-10-10 14:06:07 -0300

    windows: add script to install emacs and other tools in msys2

---
+++ windows_misc/msys2-install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+pacman -S \
+msys/man-db \
+msys/sshpass \
+msys/unzip \
+msys/zip \
+mingw64/mingw-w64-x86_64-clang \
+mingw64/mingw-w64-x86_64-cppcheck \
+mingw64/mingw-w64-x86_64-diffutils \
+mingw64/mingw-w64-x86_64-emacs \
+mingw64/mingw-w64-x86_64-global \
+mingw64/mingw-w64-x86_64-grep \
+mingw64/mingw-w64-x86_64-hunspell \
+mingw64/mingw-w64-x86_64-hunspell-en \
+mingw64/mingw-w64-x86_64-ripgrep
