7bea834aa9c213bf041434a56763c4a113645650
Leandro Cardoso <leandrocardoso@gmail.com>
2023-10-30 21:53:56 -0300

    save buffer-display-time with deskop-save

--- lisp/init-desktop.el
+++ lisp/init-desktop.el
@@ -1,4 +1,5 @@
 (with-eval-after-load "desktop"
   (setq desktop-save 'ask-if-exists)
   (desktop-save-mode)
+  (add-to-list 'desktop-locals-to-save 'buffer-display-time)
   (add-hook 'desktop-after-read-hook 'clean-buffer-list))
