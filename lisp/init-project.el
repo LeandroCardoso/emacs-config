a697ad4a8754e4ff399d2888e505ba205e5a81ec
Leandro Cardoso <leandro.cardoso@capgemini.com>
2024-02-27 19:37:09 -0300

    project: added project-info function

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -3,6 +3,12 @@
   (setq project-vc-merge-submodules nil)
 
   ;; functions
+  (defun project-info ()
+    "Display the project instance of the current buffer."
+    (interactive)
+    (let ((pr (flatten-list (project-current))))
+      (message "%s" (mapconcat 'prin1-to-string (nconc (butlast pr) (list ':) (last pr)) " "))))
+
   (defun project-query-regexp ()
     "Find all matches for REGEXP in the current project.
 
@@ -12,5 +18,6 @@ See `project-find-regexp'."
       (call-interactively 'project-find-regexp)))
 
   ;; keys
-  (global-set-key (kbd "C-x p q") 'project-query-regexp)
+  (define-key project-prefix-map "i" 'project-info)
+  (define-key project-prefix-map "q" 'project-query-regexp)
   (global-set-key (kbd "C-M-g") 'project-query-regexp))
