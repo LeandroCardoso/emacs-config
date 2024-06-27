72e4719cd8395e6cf1e32365ffc3885c98578af8
Leandro Cardoso <leandro.cardoso@capgemini.com>
2024-06-27 14:59:59 -0300

    git-link: add key bindings

---
+++ lisp/init-git-link.el
@@ -0,0 +1,10 @@
+(when (require 'git-link nil t)
+  (defvar git-link-keymap nil "Keymap for git-link commands")
+  (setq git-link-keymap
+        (let ((map (make-sparse-keymap)))
+          (define-key map "l" 'git-link)
+          (define-key map "c" 'git-link-commit)
+          (define-key map "h" 'git-link-homepage)
+          map))
+  (defalias 'git-link-keymap git-link-keymap)
+  (global-set-key (kbd "C-c l") 'git-link-keymap))
