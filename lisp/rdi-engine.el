5c7abb1cd920c93fe7d3fc0b7a4d16a7175d43a9
Leandro Cardoso <leandro.cardoso@capgemini.com>
2024-11-19 18:10:25 -0300

    rdi-engine: swap keybindings rdi/mcd

--- lisp/rdi-engine.el
+++ lisp/rdi-engine.el
@@ -2,10 +2,11 @@
 (when (require 'engine-mode nil t)
   (defengine jira-rdi
     "https://jira.rdisoftware.com/jira/secure/QuickSearch.jspa?searchString=%s"
-    :keybinding "j")
+    :keybinding "J")
   (defengine jira-mcd
     "https://mcd-tools.atlassian.net/jira/search?searchString=%s"
-    :keybinding "J"))
-;; Local Variables:
+    :keybinding "j"))
+;; Local Variables
+:
 ;; no-native-compile: t
 ;; End:
