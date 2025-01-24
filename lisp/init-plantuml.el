53b6dc3fc6c7e573762e6bae95e3d583170af590
Leandro Cardoso <leandro.cardoso@capgemini.com>
2025-01-23 23:23:28 -0300

    added plantuml

---
+++ lisp/init-plantuml.el
@@ -0,0 +1,15 @@
+(when (require 'plantuml-mode nil t)
+  (setq plantuml-indent-level 4)
+  
+  (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
+
+  ;; Workaround for fix plantuml server url encode. See
+  ;; https://github.com/skuro/plantuml-mode/pull/172/commits/88ec2b989b9e7c8ccef5d9c3aa2800758c24e7f5
+
+  (defun plantuml-server-encode-url (string)
+  "Encode the string STRING into a URL suitable for PlantUML server interactions."
+  (let* ((coding-system (or buffer-file-coding-system
+                            "utf8"))
+         (str (encode-coding-string string coding-system))
+         (encoded-string (mapconcat (lambda(x)(format "%02X" x)) str ) ))
+    (concat plantuml-server-url "/" plantuml-output-type "/~h" encoded-string))))
