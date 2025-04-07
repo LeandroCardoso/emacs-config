6a2c4d93ffb571193d545dafe415e61d18bdee25
Leandro Cardoso <leandro.cardoso@capgemini.com>
2025-04-07 19:49:05 -0300

    elisp: enable flymake

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -2,4 +2,6 @@
 (require 'mode-local)
 (setq-mode-local emacs-lisp-mode sentence-end-double-space t)
 
+(add-hook 'emacs-lisp-mode-hook 'flymake-mode)
+
 (define-key emacs-lisp-mode-map (kbd "C-c C-c") 'eval-defun) ; eval-defun is also in C-M-x
