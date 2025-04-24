ab26b99ed2b849d41a71901cb7b66fd0ba5bcf5e
Leandro Cardoso <leandro.cardoso@capgemini.com>
2025-04-24 10:51:00 -0300

    dashboard-desktop: improved documentation

--- lisp/dashboard-desktop.el
+++ lisp/dashboard-desktop.el
@@ -1,4 +1,4 @@
-;;; dashboard-desktop.el --- Integrate desktop with dashboard -*- lexical-binding:t -*-
+;;; dashboard-desktop.el --- Integrate desktop.el with dashboard.el -*- lexical-binding:t -*-
 
 ;;; Copyright: Leandro Cardoso
 
@@ -6,9 +6,20 @@
 
 ;;; Commentary:
 
-;; A 'desktop' item must be manually added to the 'dashboard-items variable:
+;; To use this package a 'desktop' item must be manually added to the `dashboard-items' variable:
 ;;
 ;; (add-to-list 'dashboard-items '(desktop . 5))
+
+;; Note: loading this package causes the 'desktop' package to be loaded, which may cause a desktop
+;; session to be automatically loaded by the 'desktop' package itself when the `desktop-save-mode'
+;; is enabled on Emacs initialization.  If this behavior is undesirable, my recommendation is to
+;; only enable the `desktop-save-mode' after a desktop session is loaded:
+;;
+;; (defun desktop-save-mode-on ()
+;;     "Enable `desktop-save-mode'.  Provided for use in hooks."
+;;     (desktop-save-mode 1))
+;; 
+;; (add-hook 'desktop-after-read-hook 'desktop-save-mode-on)
 
 ;;; Code:
 
@@ -16,23 +27,23 @@
 (require 'desktop)
 
 (defun dashboard-insert-desktop (list-size)
-    "Add a list of LIST-SIZE items of desktop files to load."
-    (dashboard-insert-section
-     "Desktop:"
-     (dashboard-shorten-paths
-      (dashboard-subseq (remq nil
-                             (mapcar #'(lambda (path)
-                                         (if (file-exists-p (concat path desktop-base-file-name))
-                                             path))
-                                     desktop-path))
-                        list-size)
-      'dashboard-desktop-alist 'desktop)
-     list-size
-     'desktop
-     (dashboard-get-shortcut 'desktop)
-     `(lambda (&rest _)
-        (desktop-read (dashboard-expand-path-alist ,el dashboard-desktop-alist)))
-     (format "%s" (dashboard-expand-path-alist el dashboard-desktop-alist))))
+  "Add a list of LIST-SIZE items of desktop files to load."
+  (dashboard-insert-section
+   "Desktop:"
+   (dashboard-shorten-paths
+    (dashboard-subseq (remq nil
+                            (mapcar #'(lambda (path)
+                                        (if (file-exists-p (concat path desktop-base-file-name))
+                                            path))
+                                    desktop-path))
+                      list-size)
+    'dashboard-desktop-alist 'desktop)
+   list-size
+   'desktop
+   (dashboard-get-shortcut 'desktop)
+   `(lambda (&rest _)
+      (desktop-read (dashboard-expand-path-alist ,el dashboard-desktop-alist)))
+   (format "%s" (dashboard-expand-path-alist el dashboard-desktop-alist))))
 
 ;; FIXME
 ;; (add-to-list 'dashboard-heading-icons
