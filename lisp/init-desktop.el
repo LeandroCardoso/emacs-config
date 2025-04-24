dc4a6b6a6c7ac5f6b36b7fce7198fa2b5374c94f
Leandro Cardoso <leandro.cardoso@capgemini.com>
2025-04-24 10:51:58 -0300

    desktop: move to use-package and only enable desktop-save-mode after a desktop-read

--- lisp/init-desktop.el
+++ lisp/init-desktop.el
@@ -1,5 +1,14 @@
-(with-eval-after-load "desktop"
-  (setq desktop-save 'ask-if-exists)
-  (desktop-save-mode)
+(use-package desktop
+  :config
+  (defun desktop-save-mode-on ()
+    "Enable `desktop-save-mode'.  Provided for use in hooks."
+    (desktop-save-mode 1))
+
+  (setopt desktop-save 'ask-if-exists)
   (add-to-list 'desktop-locals-to-save 'buffer-display-time)
-  (add-hook 'desktop-after-read-hook 'clean-buffer-list))
+
+  :hook
+  (desktop-after-read . clean-buffer-list)
+  ;; I am enabling the desktop-save-mode *after* a desktop session is loaded by the `desktop-read'
+  ;; command to avoid loading a desktop session on Emacs initialization
+  (desktop-after-read . desktop-save-mode-on))
