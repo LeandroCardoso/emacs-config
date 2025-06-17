176b69fb57d6e82d6a04ea274a40a777f3bdeb2a
Leandro Cardoso <leandro.cardoso@capgemini.com>
2025-06-17 11:15:40 -0300

    plantuml: removed workaround as bug was fixed

--- init.el
+++ init.el
@@ -1343,19 +1343,7 @@ the plist used as a communication channel."
   (setopt plantuml-jar-path (let ((plantuml-dir (expand-file-name "plantuml" user-emacs-directory)))
                               (when (file-directory-p plantuml-dir)
                                 (car (directory-files plantuml-dir t "plantuml.*jar")))))
-  (setopt plantuml-default-exec-mode (if plantuml-jar-path 'jar 'server))
-
-  ;; Workaround for fix plantuml server url encode. See
-  ;; https://github.com/skuro/plantuml-mode/pull/172/commits/88ec2b989b9e7c8ccef5d9c3aa2800758c24e7f5
-  (defun plantuml-server-encode-url-override (string)
-    "Encode the string STRING into a URL suitable for PlantUML server interactions."
-    (let* ((coding-system (or buffer-file-coding-system
-                              "utf8"))
-           (str (encode-coding-string string coding-system))
-           (encoded-string (mapconcat (lambda(x)(format "%02X" x)) str ) ))
-      (concat plantuml-server-url "/" plantuml-output-type "/~h" encoded-string)))
-
-  (advice-add 'plantuml-server-encode-url :override 'plantuml-server-encode-url-override))
+  (setopt plantuml-default-exec-mode (if plantuml-jar-path 'jar 'server)))
 
 (use-package prescient
   :ensure t
