8759e9bff8088a8a23f80b10fa7277d712332916
Leandro Cardoso <leandro.cardoso@capgemini.com>
2025-07-21 16:37:14 -0300

    windows: fix msys2-install.sh

--- windows/share/misc/msys2-install.sh
+++ windows/share/misc/msys2-install.sh
@@ -3,9 +3,9 @@ pacman -S \
 msys/man-db \
 msys/unzip \
 msys/zip \
+ucrt64/mingw-w64-ucrt-x86_64-ca-certificates \
 ucrt64/mingw-w64-ucrt-x86_64-clang \
 ucrt64/mingw-w64-ucrt-x86_64-clang-tools-extra \
-ucrt64/mingw-w64-python-codespell \
 ucrt64/mingw-w64-ucrt-x86_64-cppcheck \
 ucrt64/mingw-w64-ucrt-x86_64-diffutils \
 ucrt64/mingw-w64-ucrt-x86_64-emacs \
@@ -13,5 +13,6 @@ ucrt64/mingw-w64-ucrt-x86_64-global \
 ucrt64/mingw-w64-ucrt-x86_64-grep \
 ucrt64/mingw-w64-ucrt-x86_64-hunspell \
 ucrt64/mingw-w64-ucrt-x86_64-hunspell-en \
+ucrt64/mingw-w64-ucrt-x86_64-python-codespell \
 ucrt64/mingw-w64-ucrt-x86_64-python-tldr \
 ucrt64/mingw-w64-ucrt-x86_64-ripgrep
