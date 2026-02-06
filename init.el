110061bc80a2832246924dd1921464e3ecb0e2a9
Leandro Cardoso <leandro.cardoso@capgemini.com>
2026-02-06 16:31:19 -0300

    fix initialization on windows

--- init.el
+++ init.el
@@ -1886,7 +1886,6 @@ See `byte-recompile-and-cleanup-directory'."
 (use-package w32-extra
   :if system-windows-p
   :demand t
-  :after (xref gtags-mode)
   :config
   ;; Root directories are added in the beginning
   (w32-add-unix-root-dir "c:/msys64/ucrt64")
@@ -1908,10 +1907,12 @@ See `byte-recompile-and-cleanup-directory'."
 
   ;; Use bash shell when calling grep/ripgrep (xref) and global (gtags-mode), because it fixes the
   ;; annoying "^M" that can be displayed at end of lines.
-  (advice-add 'xref-matches-in-files :around 'with-bash-shell)
-  (advice-add 'xref-matches-in-directory :around 'with-bash-shell)
-  (advice-add 'gtags-mode--exec-sync :around 'with-bash-shell)
-  (advice-add 'gtags-mode--exec-async :around 'with-bash-shell)
+  (with-eval-after-load "xref"
+    (advice-add 'xref-matches-in-files :around 'with-bash-shell)
+    (advice-add 'xref-matches-in-directory :around 'with-bash-shell))
+  (with-eval-after-load "gtags-mode"
+    (advice-add 'gtags-mode--exec-sync :around 'with-bash-shell)
+    (advice-add 'gtags-mode--exec-async :around 'with-bash-shell))
 
   ;; map AltGr to Alt using AutoHotKey
   (if (executable-find "altgr2alt")
