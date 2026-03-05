ad81c370867c2ba9e875cad15a2c66a8d813dadc
Leandro Cardoso <leandrocardoso@gmail.com>
2026-03-04 23:54:08 -0300

    volatile-highlights: use forked repository

--- init.el
+++ init.el
@@ -1608,16 +1608,8 @@ See `tide-tsserver-executable'."
 
 (use-package volatile-highlights
   :ensure t
+  :vc (:url "git@github.com:LeandroCardoso/volatile-highlights.el.git")
   :config
-  (vhl/define-extension 'transpose 'transpose-chars 'transpose-words
-                        'transpose-lines 'transpose-sexps
-                        'transpose-sentences 'transpose-paragraphs
-                        'transpose-regions)
-  (vhl/install-extension 'transpose)
-
-  (vhl/define-extension 'duplicate 'duplicate-line 'duplicate-dwim)
-  (vhl/install-extension 'duplicate)
-
   (with-eval-after-load "move-dup"
     (vhl/define-extension 'move-dup 'move-dup-duplicate-down 'move-dup-duplicate-up
                           'move-dup-move-line 'move-dup-move-lines-down 'move-dup-move-lines-up
